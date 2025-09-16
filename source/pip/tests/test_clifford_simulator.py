3200816b6c99cb0ad2aa387ff5985916c9473b76
Ian Davis <ian.f.davis@microsoft.com>
2025-09-16 14:41:35 -0700

    Adding clifford decomp pass

--- source/pip/tests/test_clifford_simulator.py
+++ source/pip/tests/test_clifford_simulator.py
@@ -2,7 +2,13 @@
 # Licensed under the MIT License.
 
 from pathlib import Path
+import time
+import sys
+
 import qsharp
+from qsharp._simulation import run_qir, NoiseConfig
+from qsharp.passes import transform_to_clifford
+from qsharp import TargetProfile
 
 current_file_path = Path(__file__)
 # Get the directory of the current file
@@ -20,35 +26,33 @@ def read_file_relative(file_name: str) -> str:
 
 
 def test_smoke():
-    from qsharp._simulation import run_qir, NoiseConfig
-    from qsharp import TargetProfile
-
     qsharp.init(target_profile=TargetProfile.Base)
     qsharp.eval(read_file_relative("CliffordIsing.qs"))
 
-    ir = qsharp.compile("IsingModel2DEvolution(5, 5, PI() / 2.0, PI() / 2.0, 40.0, 40)")
-    output = run_qir(str(ir), 10, NoiseConfig())
+    input = qsharp.compile(
+        "IsingModel2DEvolution(5, 5, PI() / 2.0, PI() / 2.0, 40.0, 40)"
+    )
+    input = transform_to_clifford(input)
+    output = run_qir(str(input), 10, NoiseConfig())
     print(output)
 
 
-def test_1224_clifford_ising_1MM_calls():
-    from qsharp._simulation import run_qir, NoiseConfig
-    from qsharp import TargetProfile
-
+def test_1224_clifford_ising():
     qsharp.init(target_profile=TargetProfile.Base)
     qsharp.eval(read_file_relative("CliffordIsing.qs"))
 
-    ir = qsharp.compile(
-        "IsingModel2DEvolution(34, 36, PI() / 2.0, PI() / 2.0, 300.0, 300)"
+    input = qsharp.compile(
+        "IsingModel2DEvolution(34, 36, PI() / 2.0, PI() / 2.0, 40.0, 40)"
     )
-    output = run_qir(str(ir), 1, NoiseConfig())
+    input = transform_to_clifford(input)
+    qir = str(input)
+
+    output = run_qir(qir, 1, NoiseConfig())
+
     print(output)
 
 
 def test_million():
-    from qsharp._simulation import run_qir, NoiseConfig
-    from qsharp import TargetProfile
-
     qsharp.init(target_profile=TargetProfile.Base)
     qsharp.eval(read_file_relative("CliffordCalls.qs"))
 
