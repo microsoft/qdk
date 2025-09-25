621fbf65f5da33ffcd882b056649d3fc3c8d774a
Scott Carda <Scott.Carda@microsoft.com>
2025-09-25 10:37:48 -0700

    skip the new notebook from integration tests

--- build.py
+++ build.py
@@ -646,7 +646,7 @@ if build_jupyterlab:
 
 if build_pip and build_widgets and build_qdk and args.integration_tests:
     step_start("Running notebook samples integration tests")
-    # Find all notebooks in the samples directory. Skip the basic sample and the azure submission sample, since those won't run
+    # Find all notebooks in the samples directory. Skip the some of the samples since these won't run
     # nicely in automation.
     notebook_files = [
         os.path.join(dp, f)
@@ -658,6 +658,7 @@ if build_pip and build_widgets and build_qdk and args.integration_tests:
             or f.startswith("azure_submission.")
             or f.startswith("circuits.")
             or f.startswith("iterative_phase_estimation.")
+            or f.startswith("qdk.")
             or f.startswith("repeat_until_success.")
             or f.startswith("python-deps.")
         )
@@ -690,18 +691,6 @@ if build_pip and build_widgets and build_qdk and args.integration_tests:
     ]
     subprocess.run(
         pip_install_args, check=True, text=True, cwd=widgets_src, env=pip_env
-    )
-
-    # Install the qdk package from the folder so dependencies are installed properly
-    pip_install_args = [
-        python_bin,
-        "-m",
-        "pip",
-        "install",
-        qdk_python_src,
-    ]
-    subprocess.run(
-        pip_install_args, check=True, text=True, cwd=qdk_python_src, env=pip_env
     )
 
     # Install other dependencies
