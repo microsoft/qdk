73daa5ea93ca186e1ed9f93e201fb898ef4c42da
Scott Carda <Scott.Carda@microsoft.com>
2025-09-30 12:03:15 -0700

    refactored tests

--- source/language_service/src/wrapper_refactor/tests.rs
+++ source/language_service/src/wrapper_refactor/tests.rs
@@ -1,266 +1,334 @@
 // Copyright (c) Microsoft Corporation.
+// Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-//! Tests for the wrapper refactor code action logic.
+//! Tests for the wrapper refactor code action logic (synchronous style similar to hover tests).
 
-use crate::tests::test_fs::{FsNode, TestProjectHost, dir, file};
-use crate::{
-    Encoding, LanguageService, UpdateWorker,
-    protocol::{DiagnosticUpdate, ErrorKind, TestCallables},
-};
-use std::{cell::RefCell, rc::Rc};
+use crate::{code_action, test_utils::compile_project_with_markers_no_cursor};
+use expect_test::expect;
+use qsc::line_column::{Encoding, Position, Range};
 
-// Local copy of helper to create update worker (cannot import private symbol from parent tests module directly).
-fn create_update_worker<'a>(
-    ls: &mut LanguageService,
-    received_errors: &'a RefCell<
-        Vec<(
-            String,
-            Option<u32>,
-            Vec<qsc::compile::ErrorKind>,
-            Vec<qsc::project::Error>,
-        )>,
-    >,
-    received_test_cases: &'a RefCell<Vec<TestCallables>>,
-) -> UpdateWorker<'a> {
-    ls.create_update_worker(
-        |update: DiagnosticUpdate| {
-            let project_errors = update.errors.iter().filter_map(|error| match error {
-                ErrorKind::Project(error) => Some(error.clone()),
-                ErrorKind::Compile(_) | ErrorKind::DocumentStatus { .. } => None,
-            });
-            let compile_errors = update.errors.iter().filter_map(|error| match error {
-                ErrorKind::Compile(error) => Some(error.error().clone()),
-                ErrorKind::Project(_) | ErrorKind::DocumentStatus { .. } => None,
-            });
-
-            let mut v = received_errors.borrow_mut();
-            v.push((
-                update.uri,
-                update.version,
-                compile_errors.collect(),
-                project_errors.collect(),
-            ));
-        },
-        move |update: TestCallables| {
-            let mut v = received_test_cases.borrow_mut();
-            v.push(update);
-        },
-        TestProjectHost {
-            fs: TEST_FS.with(Clone::clone),
-        },
-    )
-}
-
-thread_local! { static TEST_FS: Rc<RefCell<FsNode>> = Rc::new(RefCell::new(test_fs())) }
-
-fn test_fs() -> FsNode {
-    FsNode::Dir(
-        [dir(
-            "project",
-            [
-                file("qsharp.json", "{}"),
-                dir(
-                    "src",
-                    [
-                        file(
-                            "other_file.qs",
-                            "namespace OtherFile { operation Other() : Unit {} }",
-                        ),
-                        file("this_file.qs", "namespace Foo { }"),
-                    ],
-                ),
-            ],
-        )]
-        .into_iter()
-        .collect(),
-    )
-}
-
-// Helper to fetch a specific wrapper action and return its inserted text
-async fn get_wrapper_text(source: &str, op_name: &str) -> String {
-    let received_errors = RefCell::new(Vec::new());
-    let test_cases = RefCell::new(Vec::new());
-    let mut ls = LanguageService::new(Encoding::Utf8);
-    let mut worker = create_update_worker(&mut ls, &received_errors, &test_cases);
-
-    ls.update_document("test.qs", 1, source, "qsharp");
-    worker.apply_pending().await;
-
+fn get_wrapper_text(source: &str, op_name: &str) -> String {
+    let (compilation, _targets) =
+        compile_project_with_markers_no_cursor(&[("<source>", source)], false);
     let newline_count = u32::try_from(source.matches('\n').count()).expect("count fits");
-    let end_pos = if newline_count == 0 {
-        qsc::line_column::Position {
+    let end = if newline_count == 0 {
+        Position {
             line: 0,
-            column: u32::try_from(source.len()).expect("length fits"),
+            column: u32::try_from(source.len()).expect("len fits"),
         }
     } else {
-        qsc::line_column::Position {
+        Position {
             line: newline_count,
             column: 0,
         }
     };
-    let range = qsc::line_column::Range {
-        start: qsc::line_column::Position { line: 0, column: 0 },
-        end: end_pos,
+    let range = Range {
+        start: Position { line: 0, column: 0 },
+        end,
     };
-    let actions = ls.get_code_actions("test.qs", range);
+    let actions = code_action::get_code_actions(&compilation, "<source>", range, Encoding::Utf8);
     let action = actions
         .iter()
         .find(|a| a.title == format!("Generate wrapper for {op_name}"))
-        .unwrap_or_else(|| panic!("Expected wrapper action for {op_name}"));
+        .unwrap_or_else(|| {
+            panic!(
+                "Expected wrapper action for {op_name}. Available: {:?}",
+                actions.iter().map(|a| &a.title).collect::<Vec<_>>()
+            )
+        });
     action.edit.as_ref().expect("expected edit").changes[0].1[0]
         .new_text
         .clone()
 }
 
-#[tokio::test]
-async fn code_action_operation_refactor() {
-    let wrapper_text = get_wrapper_text("operation Op(a : Int, b : Bool) : Unit { }", "Op").await;
-    assert!(wrapper_text.contains("operation Op_Wrapper() : Unit {"));
+#[test]
+fn basic_wrapper() {
+    // Wrap in a namespace since most language features (including item collection) assume a namespace context.
+    let wrapper_text = get_wrapper_text(
+        "namespace Test { operation Op(a : Int, b : Bool) : Unit { } }",
+        "Op",
+    );
+    expect![[r#"
+        operation Op_Wrapper() : Unit {
+            // TODO: Fill out the values for the parameters
+            let a = 0;
+            let b = false;
+
+            // Call original operation
+            Op(a, b);
+        }
+
+    "#]]
+    .assert_eq(&wrapper_text);
 }
 
-#[tokio::test]
-async fn code_action_wrapper_indentation_nested() {
+#[test]
+fn indentation_nested() {
     let source =
         "namespace Test {\n    // Some preceding code\n    operation Ind(a : Int) : Unit { }\n}";
-    let wrapper_text = get_wrapper_text(source, "Ind").await;
+    let wrapper_text = get_wrapper_text(source, "Ind");
     assert!(wrapper_text.starts_with("operation Ind_Wrapper()"));
     assert!(wrapper_text.contains("        // Call original operation"));
 }
 
-#[tokio::test]
-async fn code_action_wrapper_indentation_tabs() {
+#[test]
+fn indentation_tabs() {
     let source = "namespace Test {\n\toperation Tabbed(a : Int) : Unit { }\n}";
-    let wrapper_text = get_wrapper_text(source, "Tabbed").await;
+    let wrapper_text = get_wrapper_text(source, "Tabbed");
     assert!(wrapper_text.starts_with("operation Tabbed_Wrapper()"));
     assert!(wrapper_text.contains("\t\t// Call original operation"));
 }
 
-#[tokio::test]
-async fn code_action_wrapper_default_qubit() {
-    let source = "namespace Test { operation Q(q : Qubit) : Unit {} }";
-    let wrapper = get_wrapper_text(source, "Q").await;
-    assert!(wrapper.contains("use q = Qubit();"));
+#[test]
+fn default_qubit() {
+    let wrapper = get_wrapper_text("namespace Test { operation Q(q : Qubit) : Unit { } }", "Q");
+    expect![[r#"
+        operation Q_Wrapper() : Unit {
+            // TODO: Fill out the values for the parameters
+            use q = Qubit();
+
+            // Call original operation
+            Q(q);
+        }
+
+    "#]]
+    .assert_eq(&wrapper);
 }
 
-#[tokio::test]
-async fn code_action_wrapper_default_qubit_array() {
-    let source = "namespace Test { operation QA(qs : Qubit[]) : Unit {} }";
-    let wrapper = get_wrapper_text(source, "QA").await;
-    assert!(wrapper.contains("use qs = Qubit[1];"));
+#[test]
+fn default_qubit_array() {
+    let wrapper = get_wrapper_text(
+        "namespace Test { operation QA(qs : Qubit[]) : Unit { } }",
+        "QA",
+    );
+    expect![[r#"
+        operation QA_Wrapper() : Unit {
+            // TODO: Fill out the values for the parameters
+            use qs = Qubit[1];
+
+            // Call original operation
+            QA(qs);
+        }
+
+    "#]]
+    .assert_eq(&wrapper);
 }
 
-#[tokio::test]
-async fn code_action_wrapper_default_primitives() {
-    let source = "namespace Test { operation Prims(a : Int, b : Bool, c : Double, d : Result, e : Pauli, f : BigInt, g : String) : Unit {} }";
-    let wrapper = get_wrapper_text(source, "Prims").await;
-    for expected in [
-        "let a = 0;",
-        "let b = false;",
-        "let c = 0.0;",
-        "let d = Zero;",
-        "let e = PauliI;",
-        "let f = 0L;",
-        "let g = \"\";",
-    ] {
-        assert!(
-            wrapper.contains(expected),
-            "missing primitive default line: {expected}\nFull text: {wrapper}"
-        );
-    }
+#[test]
+fn default_primitives() {
+    let wrapper = get_wrapper_text(
+        "namespace Test { operation Prims(a : Int, b : Bool, c : Double, d : Result, e : Pauli, f : BigInt, g : String) : Unit { } }",
+        "Prims",
+    );
+    expect![[r#"
+        operation Prims_Wrapper() : Unit {
+            // TODO: Fill out the values for the parameters
+            let a = 0;
+            let b = false;
+            let c = 0.0;
+            let d = Zero;
+            let e = PauliI;
+            let f = 0L;
+            let g = "";
+
+            // Call original operation
+            Prims(a, b, c, d, e, f, g);
+        }
+
+    "#]]
+    .assert_eq(&wrapper);
 }
 
-#[tokio::test]
-async fn code_action_wrapper_default_udt() {
-    let source = "namespace Test { newtype MyT = Int; operation UsesUdt(x : MyT) : Unit {} }";
-    let wrapper = get_wrapper_text(source, "UsesUdt").await;
-    assert!(wrapper.contains("// TODO: provide value for x (UDT MyT)"));
+#[test]
+fn default_udt() {
+    let wrapper = get_wrapper_text(
+        "namespace Test { newtype MyT = Int; operation UsesUdt(x : MyT) : Unit { } }",
+        "UsesUdt",
+    );
+    expect![[r#"
+        operation UsesUdt_Wrapper() : Unit {
+            // TODO: Fill out the values for the parameters
+            // TODO: provide value for x (UDT MyT)
+
+            // Call original operation
+            UsesUdt(_);
+        }
+
+    "#]]
+    .assert_eq(&wrapper);
 }
 
-#[tokio::test]
-async fn code_action_wrapper_default_generic() {
-    let source = "namespace Test { operation Generic<'T>(x : 'T) : Unit {} }";
-    let wrapper = get_wrapper_text(source, "Generic").await;
-    assert!(wrapper.contains("// TODO: provide value for x (Generic parameter"));
+#[test]
+fn default_generic() {
+    let wrapper = get_wrapper_text(
+        "namespace Test { operation Generic<'T>(x : 'T) : Unit { } }",
+        "Generic",
+    );
+    expect![[r#"
+        operation Generic_Wrapper() : Unit {
+            // TODO: Fill out the values for the parameters
+            // TODO: provide value for x (Generic parameter 'T)
+
+            // Call original operation
+            Generic(_);
+        }
+
+    "#]]
+    .assert_eq(&wrapper);
 }
 
-#[tokio::test]
-async fn code_action_wrapper_default_array_int() {
-    let source = "namespace Test { operation Arr(arr : Int[]) : Unit {} }";
-    let wrapper = get_wrapper_text(source, "Arr").await;
-    assert!(wrapper.contains("let arr = [];"));
+#[test]
+fn default_array_int() {
+    let wrapper = get_wrapper_text(
+        "namespace Test { operation Arr(arr : Int[]) : Unit { } }",
+        "Arr",
+    );
+    expect![[r#"
+        operation Arr_Wrapper() : Unit {
+            // TODO: Fill out the values for the parameters
+            let arr = [];
+
+            // Call original operation
+            Arr(arr);
+        }
+
+    "#]]
+    .assert_eq(&wrapper);
 }
 
-#[tokio::test]
-async fn code_action_wrapper_default_tuple_destructured() {
-    let source =
-        "namespace Test { operation Tup(param : (Int, Bool, (Double, Qubit))) : Unit { } }";
-    let wrapper = get_wrapper_text(source, "Tup").await;
-    assert!(wrapper.contains("let param = ("));
-    assert!(wrapper.contains("0, false, (0.0, "));
-    assert!(wrapper.contains("use param_q"));
-    assert!(wrapper.contains("Tup(param);"));
+#[test]
+fn default_tuple_destructured() {
+    let wrapper = get_wrapper_text(
+        "namespace Test { operation Tup(param : (Int, Bool, (Double, Qubit))) : Unit { } }",
+        "Tup",
+    );
+    expect![[r#"
+        operation Tup_Wrapper() : Unit {
+            // TODO: Fill out the values for the parameters
+            use param_q0 = Qubit();
+            let param = (0, false, (0.0, param_q0));
+
+            // Call original operation
+            Tup(param);
+        }
+
+    "#]]
+    .assert_eq(&wrapper);
 }
 
-#[tokio::test]
-async fn code_action_wrapper_default_tuple_bound() {
-    let source = "namespace Test { operation Tup2(t : (Qubit, Int, (Bool, Qubit[]))) : Unit { } }";
-    let wrapper = get_wrapper_text(source, "Tup2").await;
-    assert!(wrapper.contains("use t_q0 = Qubit();"));
-    assert!(wrapper.contains("let t = (t_q0, 0, (false,"));
-    assert!(wrapper.contains("use t_qs"));
+#[test]
+fn default_tuple_bound() {
+    let wrapper = get_wrapper_text(
+        "namespace Test { operation Tup2(t : (Qubit, Int, (Bool, Qubit[]))) : Unit { } }",
+        "Tup2",
+    );
+    expect![[r#"
+        operation Tup2_Wrapper() : Unit {
+            // TODO: Fill out the values for the parameters
+            use t_q0 = Qubit();
+            use t_qs0 = Qubit[1];
+            let t = (t_q0, 0, (false, t_qs0));
+
+            // Call original operation
+            Tup2(t);
+        }
+
+    "#]]
+    .assert_eq(&wrapper);
 }
 
-#[tokio::test]
-async fn code_action_wrapper_qubit_tuple_counter_persistence() {
-    let source = "namespace Test { operation Deep(t : (Qubit, (Qubit, Qubit), Qubit, (Qubit, Qubit), (Qubit, (Qubit, Qubit)))) : Unit { } }";
-    let wrapper = get_wrapper_text(source, "Deep").await;
-    for name in [
-        "t_q0", "t_q1", "t_q2", "t_q3", "t_q4", "t_q5", "t_q6", "t_q7",
-    ] {
-        assert!(
-            wrapper.contains(&format!("use {name} = Qubit();")),
-            "Missing expected allocation {name}. Wrapper:\n{wrapper}"
-        );
-    }
+#[test]
+fn qubit_tuple_counter_persistence() {
+    let wrapper = get_wrapper_text(
+        "namespace Test { operation Deep(t : (Qubit, (Qubit, Qubit), Qubit, (Qubit, Qubit), (Qubit, (Qubit, Qubit)))) : Unit { } }",
+        "Deep",
+    );
+    expect![[r#"
+        operation Deep_Wrapper() : Unit {
+            // TODO: Fill out the values for the parameters
+            use t_q0 = Qubit();
+            use t_q1 = Qubit();
+            use t_q2 = Qubit();
+            use t_q3 = Qubit();
+            use t_q4 = Qubit();
+            use t_q5 = Qubit();
+            use t_q6 = Qubit();
+            use t_q7 = Qubit();
+            use t_q8 = Qubit();
+            let t = (t_q0, (t_q1, t_q2), t_q3, (t_q4, t_q5), (t_q6, (t_q7, t_q8)));
+
+            // Call original operation
+            Deep(t);
+        }
+
+    "#]]
+    .assert_eq(&wrapper);
 }
 
-#[tokio::test]
-async fn code_action_wrapper_qubit_and_array_counters() {
-    let source = "namespace Test { operation Mix(t : (Qubit, Qubit[], (Qubit, Qubit[], Qubit[]), Qubit, Qubit[])) : Unit { } }";
-    let wrapper = get_wrapper_text(source, "Mix").await;
-    assert!(wrapper.contains("use t_q0 = Qubit();"));
-    assert!(wrapper.contains("use t_q1 = Qubit();"));
-    assert!(wrapper.contains("use t_q2 = Qubit();"));
-    assert!(wrapper.contains("use t_qs0 = Qubit[1];"));
-    assert!(wrapper.contains("use t_qs1 = Qubit[1];"));
-    assert!(wrapper.contains("use t_qs2 = Qubit[1];"));
+#[test]
+fn qubit_and_array_counters() {
+    let wrapper = get_wrapper_text(
+        "namespace Test { operation Mix(t : (Qubit, Qubit[], (Qubit, Qubit[], Qubit[]), Qubit, Qubit[])) : Unit { } }",
+        "Mix",
+    );
+    expect![[r#"
+        operation Mix_Wrapper() : Unit {
+            // TODO: Fill out the values for the parameters
+            use t_q0 = Qubit();
+            use t_qs0 = Qubit[1];
+            use t_q1 = Qubit();
+            use t_qs1 = Qubit[1];
+            use t_qs2 = Qubit[1];
+            use t_q2 = Qubit();
+            use t_qs3 = Qubit[1];
+            let t = (t_q0, t_qs0, (t_q1, t_qs1, t_qs2), t_q2, t_qs3);
+
+            // Call original operation
+            Mix(t);
+        }
+
+    "#]]
+    .assert_eq(&wrapper);
 }
 
-#[tokio::test]
-async fn code_action_wrapper_tuple_todo_positioning() {
-    let source = "namespace Test { newtype MyT = Int; operation Mixed<'T>(t : (Qubit, MyT, 'T)) : Unit { } }";
-    let wrapper = get_wrapper_text(source, "Mixed").await;
-    let alloc_index = wrapper
-        .find("use t_q0 = Qubit();")
-        .expect("allocation present");
-    let binding_index = wrapper.find("let t = (").expect("binding present");
-    assert!(alloc_index < binding_index);
-    let todo_udt_index = wrapper
-        .find("TODO: provide value for tuple component of t (UDT MyT)")
-        .expect("UDT TODO");
-    let generic_index = wrapper
-        .find("TODO: provide value for tuple component of t (Generic parameter")
-        .expect("Generic TODO");
-    assert!(alloc_index < todo_udt_index && todo_udt_index < binding_index);
-    assert!(alloc_index < generic_index && generic_index < binding_index);
+#[test]
+fn tuple_todo_positioning() {
+    let wrapper = get_wrapper_text(
+        "namespace Test { newtype MyT = Int; operation Mixed<'T>(t : (Qubit, MyT, 'T)) : Unit { } }",
+        "Mixed",
+    );
+    expect![[r#"
+        operation Mixed_Wrapper() : Unit {
+            // TODO: Fill out the values for the parameters
+            use t_q0 = Qubit();
+            // TODO: provide value for tuple component of t (UDT MyT)
+            // TODO: provide value for tuple component of t (Generic parameter 'T)
+            let t = (t_q0, _, _);
+
+            // Call original operation
+            Mixed(t);
+        }
+
+    "#]]
+    .assert_eq(&wrapper);
 }
 
-#[tokio::test]
-async fn code_action_wrapper_default_single_element_tuple() {
-    let source = "namespace Test { operation Single(t : (Double,)) : Unit { } }";
-    let wrapper = get_wrapper_text(source, "Single").await;
-    assert!(wrapper.contains("let t = (0.0,);"));
+#[test]
+fn default_single_element_tuple() {
+    let wrapper = get_wrapper_text(
+        "namespace Test { operation Single(t : (Double,)) : Unit { } }",
+        "Single",
+    );
+    expect![[r#"
+        operation Single_Wrapper() : Unit {
+            // TODO: Fill out the values for the parameters
+            let t = (0.0,);
+
+            // Call original operation
+            Single(t);
+        }
+
+    "#]]
+    .assert_eq(&wrapper);
 }
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
