86aaa7dd0c3fc823468245c3340a5781eb563521
Mathias Soeken <mathias.soeken@outlook.com>
2025-10-30 09:54:00 +0100

    Adressing reviewers' comments.

--- source/compiler/qsc_partial_eval/src/management.rs
+++ source/compiler/qsc_partial_eval/src/management.rs
@@ -150,6 +150,7 @@ impl Backend for QuantumIntrinsicsChecker {
         match name {
             "BeginEstimateCaching" => Some(Ok(Value::Bool(true))),
             "EndEstimateCaching"
+            | "EnableMemoryComputeArchitecture"
             | "GlobalPhase"
             | "ConfigurePauliNoise"
             | "ConfigureQubitLoss"
