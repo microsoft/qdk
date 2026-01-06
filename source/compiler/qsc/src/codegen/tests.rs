20c2fb1c118e553b69d042c5289d61dda9c55e76
Stefan J. Wernli <swernli@microsoft.com>
2026-01-06 10:50:54 -0800

    Update RIR reindex pass to avoid using extra qubits due to initial resets (#2829)
    This change avoids an issue with programs compiled for Base Profile that
    use initial resets on qubits and causes reindex to increase the total
    qubit count unnecessarily. Instead of treating these like the other
    resets during the program, a reset on a qubit that has not been used in
    any other operation is not reindexed and the reset is simply dropped.
    
    This additionally fixes #2575 by removing the `QubitReset` capability
    which no longer applies since qubit reset is a default part of the
    `Adaptive` capability.

--- source/compiler/qsc/src/codegen/tests.rs
+++ source/compiler/qsc/src/codegen/tests.rs
@@ -512,8 +512,8 @@ mod base_profile {
               call void @__quantum__rt__initialize(i8* null)
               call void @__quantum__qis__x__body(%Qubit* inttoptr (i64 0 to %Qubit*))
               call void @__quantum__qis__x__body(%Qubit* inttoptr (i64 0 to %Qubit*))
-              call void @__quantum__qis__m__body(%Qubit* inttoptr (i64 3 to %Qubit*), %Result* inttoptr (i64 0 to %Result*))
-              call void @__quantum__qis__m__body(%Qubit* inttoptr (i64 2 to %Qubit*), %Result* inttoptr (i64 1 to %Result*))
+              call void @__quantum__qis__m__body(%Qubit* inttoptr (i64 2 to %Qubit*), %Result* inttoptr (i64 0 to %Result*))
+              call void @__quantum__qis__m__body(%Qubit* inttoptr (i64 1 to %Qubit*), %Result* inttoptr (i64 1 to %Result*))
               call void @__quantum__rt__tuple_record_output(i64 2, i8* getelementptr inbounds ([1 x i8], [1 x i8]* @empty_tag, i64 0, i64 0))
               call void @__quantum__rt__result_record_output(%Result* inttoptr (i64 0 to %Result*), i8* getelementptr inbounds ([6 x i8], [6 x i8]* @0, i64 0, i64 0))
               call void @__quantum__rt__result_record_output(%Result* inttoptr (i64 1 to %Result*), i8* getelementptr inbounds ([6 x i8], [6 x i8]* @1, i64 0, i64 0))
@@ -530,7 +530,7 @@ mod base_profile {
 
             declare void @__quantum__qis__m__body(%Qubit*, %Result*) #1
 
-            attributes #0 = { "entry_point" "output_labeling_schema" "qir_profiles"="base_profile" "required_num_qubits"="4" "required_num_results"="2" }
+            attributes #0 = { "entry_point" "output_labeling_schema" "qir_profiles"="base_profile" "required_num_qubits"="3" "required_num_results"="2" }
             attributes #1 = { "irreversible" }
 
             ; module flags
@@ -583,7 +583,7 @@ mod adaptive_profile {
               call void @__quantum__qis__rz__body(double 2.0, %Qubit* inttoptr (i64 0 to %Qubit*))
               call void @__quantum__qis__rz__body(double 0.0, %Qubit* inttoptr (i64 0 to %Qubit*))
               call void @__quantum__qis__rz__body(double 1.0, %Qubit* inttoptr (i64 0 to %Qubit*))
-              call void @__quantum__qis__m__body(%Qubit* inttoptr (i64 0 to %Qubit*), %Result* inttoptr (i64 0 to %Result*))
+              call void @__quantum__qis__mresetz__body(%Qubit* inttoptr (i64 0 to %Qubit*), %Result* inttoptr (i64 0 to %Result*))
               call void @__quantum__rt__result_record_output(%Result* inttoptr (i64 0 to %Result*), i8* getelementptr inbounds ([4 x i8], [4 x i8]* @0, i64 0, i64 0))
               ret i64 0
             }
@@ -592,9 +592,9 @@ mod adaptive_profile {
 
             declare void @__quantum__qis__rz__body(double, %Qubit*)
 
-            declare void @__quantum__rt__result_record_output(%Result*, i8*)
+            declare void @__quantum__qis__mresetz__body(%Qubit*, %Result*) #1
 
-            declare void @__quantum__qis__m__body(%Qubit*, %Result*) #1
+            declare void @__quantum__rt__result_record_output(%Result*, i8*)
 
             attributes #0 = { "entry_point" "output_labeling_schema" "qir_profiles"="adaptive_profile" "required_num_qubits"="1" "required_num_results"="1" }
             attributes #1 = { "irreversible" }
@@ -609,57 +609,6 @@ mod adaptive_profile {
             !3 = !{i32 1, !"dynamic_result_management", i1 false}
         "#]]
         .assert_eq(&qir);
-    }
-
-    #[test]
-    fn qubit_reuse_triggers_reindexing() {
-        let source = "namespace Test {
-            @EntryPoint()
-            operation Main() : (Result, Result) {
-                use q = Qubit();
-                (MResetZ(q), MResetZ(q))
-            }
-        }";
-        let qir = compile_source_to_qir(source, *CAPABILITIES);
-        expect![[r#"
-            %Result = type opaque
-            %Qubit = type opaque
-
-            @empty_tag = internal constant [1 x i8] c"\00"
-            @0 = internal constant [6 x i8] c"0_t0r\00"
-            @1 = internal constant [6 x i8] c"1_t1r\00"
-
-            define i64 @ENTRYPOINT__main() #0 {
-            block_0:
-              call void @__quantum__rt__initialize(i8* null)
-              call void @__quantum__qis__m__body(%Qubit* inttoptr (i64 0 to %Qubit*), %Result* inttoptr (i64 0 to %Result*))
-              call void @__quantum__qis__m__body(%Qubit* inttoptr (i64 1 to %Qubit*), %Result* inttoptr (i64 1 to %Result*))
-              call void @__quantum__rt__tuple_record_output(i64 2, i8* getelementptr inbounds ([1 x i8], [1 x i8]* @empty_tag, i64 0, i64 0))
-              call void @__quantum__rt__result_record_output(%Result* inttoptr (i64 0 to %Result*), i8* getelementptr inbounds ([6 x i8], [6 x i8]* @0, i64 0, i64 0))
-              call void @__quantum__rt__result_record_output(%Result* inttoptr (i64 1 to %Result*), i8* getelementptr inbounds ([6 x i8], [6 x i8]* @1, i64 0, i64 0))
-              ret i64 0
-            }
-
-            declare void @__quantum__rt__initialize(i8*)
-
-            declare void @__quantum__rt__tuple_record_output(i64, i8*)
-
-            declare void @__quantum__rt__result_record_output(%Result*, i8*)
-
-            declare void @__quantum__qis__m__body(%Qubit*, %Result*) #1
-
-            attributes #0 = { "entry_point" "output_labeling_schema" "qir_profiles"="adaptive_profile" "required_num_qubits"="2" "required_num_results"="2" }
-            attributes #1 = { "irreversible" }
-
-            ; module flags
-
-            !llvm.module.flags = !{!0, !1, !2, !3}
-
-            !0 = !{i32 1, !"qir_major_version", i32 1}
-            !1 = !{i32 7, !"qir_minor_version", i32 0}
-            !2 = !{i32 1, !"dynamic_qubit_management", i1 false}
-            !3 = !{i32 1, !"dynamic_result_management", i1 false}
-        "#]].assert_eq(&qir);
     }
 
     #[test]
@@ -802,9 +751,9 @@ mod adaptive_profile {
             block_0:
               call void @__quantum__rt__initialize(i8* null)
               call void @__quantum__qis__x__body(%Qubit* inttoptr (i64 0 to %Qubit*))
-              call void @__quantum__qis__m__body(%Qubit* inttoptr (i64 0 to %Qubit*), %Result* inttoptr (i64 0 to %Result*))
+              call void @__quantum__qis__mresetz__body(%Qubit* inttoptr (i64 0 to %Qubit*), %Result* inttoptr (i64 0 to %Result*))
               call void @__quantum__qis__x__body(%Qubit* inttoptr (i64 1 to %Qubit*))
-              call void @__quantum__qis__m__body(%Qubit* inttoptr (i64 1 to %Qubit*), %Result* inttoptr (i64 1 to %Result*))
+              call void @__quantum__qis__mresetz__body(%Qubit* inttoptr (i64 1 to %Qubit*), %Result* inttoptr (i64 1 to %Result*))
               call void @__quantum__rt__array_record_output(i64 2, i8* getelementptr inbounds ([1 x i8], [1 x i8]* @empty_tag, i64 0, i64 0))
               call void @__quantum__rt__result_record_output(%Result* inttoptr (i64 0 to %Result*), i8* getelementptr inbounds ([6 x i8], [6 x i8]* @0, i64 0, i64 0))
               call void @__quantum__rt__result_record_output(%Result* inttoptr (i64 1 to %Result*), i8* getelementptr inbounds ([6 x i8], [6 x i8]* @1, i64 0, i64 0))
@@ -815,11 +764,11 @@ mod adaptive_profile {
 
             declare void @__quantum__qis__x__body(%Qubit*)
 
+            declare void @__quantum__qis__mresetz__body(%Qubit*, %Result*) #1
+
             declare void @__quantum__rt__array_record_output(i64, i8*)
 
             declare void @__quantum__rt__result_record_output(%Result*, i8*)
-
-            declare void @__quantum__qis__m__body(%Qubit*, %Result*) #1
 
             attributes #0 = { "entry_point" "output_labeling_schema" "qir_profiles"="adaptive_profile" "required_num_qubits"="2" "required_num_results"="2" }
             attributes #1 = { "irreversible" }
@@ -844,9 +793,7 @@ mod adaptive_ri_profile {
     use super::compile_source_to_qir;
     static CAPABILITIES: std::sync::LazyLock<TargetCapabilityFlags> =
         std::sync::LazyLock::new(|| {
-            TargetCapabilityFlags::Adaptive
-                | TargetCapabilityFlags::QubitReset
-                | TargetCapabilityFlags::IntegerComputations
+            TargetCapabilityFlags::Adaptive | TargetCapabilityFlags::IntegerComputations
         });
 
     #[test]
@@ -1339,7 +1286,6 @@ mod adaptive_rif_profile {
     static CAPABILITIES: std::sync::LazyLock<TargetCapabilityFlags> =
         std::sync::LazyLock::new(|| {
             TargetCapabilityFlags::Adaptive
-                | TargetCapabilityFlags::QubitReset
                 | TargetCapabilityFlags::IntegerComputations
                 | TargetCapabilityFlags::FloatingPointComputations
         });
