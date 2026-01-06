04b0d5cef3bd0684ca5a24726c88d38169c03c29
Stefan J. Wernli <swernli@microsoft.com>
2026-01-05 23:11:05 -0800

    Update RIR reindex pass to avoid using extra qubits due to initial resets
    This change avoids an issue with programs compiled for Base Profile that use initial resets on qubits and causes reindex to increase the total qubit count unnecessarily. Instead of treating these like the other resets during the program, a reset on a qubit that has not been used in any other operation is not reindexed and the reset is simply dropped.
    
    This additionally fixes #2575 by removing the `QubitReset` capability which no longer applies since qubit reset is a default part of the `Adaptive` capability.

--- source/compiler/qsc_passes/src/capabilitiesck/tests_adaptive.rs
+++ source/compiler/qsc_passes/src/capabilitiesck/tests_adaptive.rs
@@ -25,14 +25,6 @@ fn check_profile(source: &str, expect: &Expect) {
     check(source, expect, TargetCapabilityFlags::Adaptive);
 }
 
-fn check_profile_extended(source: &str, capabilities: TargetCapabilityFlags, expect: &Expect) {
-    check(
-        source,
-        expect,
-        TargetCapabilityFlags::Adaptive | capabilities,
-    );
-}
-
 fn check_profile_for_exe(source: &str, expect: &Expect) {
     check_for_exe(source, expect, TargetCapabilityFlags::Adaptive);
 }
@@ -477,9 +469,8 @@ fn custom_measurement_with_simulatable_intrinsic_yields_no_errors() {
 
 #[test]
 fn custom_reset_yields_no_errors() {
-    check_profile_extended(
+    check_profile(
         CUSTOM_RESET,
-        TargetCapabilityFlags::QubitReset,
         &expect![[r#"
             []
         "#]],
@@ -488,9 +479,8 @@ fn custom_reset_yields_no_errors() {
 
 #[test]
 fn custom_reset_with_simulatable_intrinsic_yields_no_errors() {
-    check_profile_extended(
+    check_profile(
         CUSTOM_RESET_WITH_SIMULATABLE_INTRINSIC_ATTR,
-        TargetCapabilityFlags::QubitReset,
         &expect![[r#"
             []
         "#]],
