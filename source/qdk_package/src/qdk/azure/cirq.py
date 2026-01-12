5bc58c35fe6167e57a442ea4a741e310c274f964
Scott Carda <Scott.Carda@microsoft.com>
2026-01-12 11:33:13 -0800

    updated wording to encourage use of `cirq` extra

--- source/qdk_package/src/qdk/azure/cirq.py
+++ source/qdk_package/src/qdk/azure/cirq.py
@@ -3,7 +3,7 @@
 
 """qdk.azure.cirq: re-export of azure.quantum.cirq symbols.
 
-Requires installation: ``pip install \"qdk[azure]\"``.
+Requires installation: ``pip install \"qdk[azure,cirq]\"``.
 
 Example:
     from qdk.azure.cirq import <symbol>
@@ -14,5 +14,5 @@ try:
     from azure.quantum.cirq import *  # pyright: ignore[reportWildcardImportFromLibrary]
 except Exception as ex:
     raise ImportError(
-        "qdk.azure.cirq requires the azure extra (and cirq). Install with 'pip install \"qdk[azure]\"'."
+        "qdk.azure.cirq requires the azure and cirq extras. Install with 'pip install \"qdk[azure,cirq]\"'."
     ) from ex
