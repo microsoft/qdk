52a9d7c15cbd42a21b6d978b639a620bf03e20fd
Bill Ticehurst <billti@microsoft.com>
2026-01-13 21:35:55 -0800

    More timeout increases for new failures

--- source/vscode/test/suites/debugger/openqasm.test.ts
+++ source/vscode/test/suites/debugger/openqasm.test.ts
@@ -50,6 +50,7 @@ suite("OpenQASM Debugger Tests", function suite() {
   });
 
   test("Launch with debugProgram command", async () => {
+    this.timeout(4000); // This test can be slow on some CI machines, so increase timeout
     await vscode.window.showTextDocument(selfContainedUri);
 
     // launch debugger
