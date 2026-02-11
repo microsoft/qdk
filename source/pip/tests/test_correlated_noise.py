03c40c828b902543fc6ddb24bf66006d041cdf09
Oscar Puente <oscarpuente@microsoft.com>
2026-02-11 09:47:17 -0800

    add missing test

--- source/pip/tests/test_correlated_noise.py
+++ source/pip/tests/test_correlated_noise.py
@@ -6,18 +6,18 @@ from qsharp._simulation import NoiseConfig, run_qir
 from qsharp import Result
 import qsharp.openqasm
 
-# Tests for the Q# noisy simulator.
+SIMULATORS = ("clifford", "cpu", "gpu")
 QASM_WITH_CORRELATED_NOISE = """
 OPENQASM 3.0;
 include "stdgates.inc";
 
 @qdk.qir.noise_intrinsic
-gate test_noise_intrinsic q0, q1 {}
+gate test_noise_intrinsic q0, q1, q2 {}
 
-qubit[2] qs;
+qubit[3] qs;
 x qs[1];
-test_noise_intrinsic qs[0], qs[1];
-bit[2] res = measure qs;
+test_noise_intrinsic qs[0], qs[1], qs[2];
+bit[3] res = measure qs;
 """
 
 QIR_WITH_CORRELATED_NOISE = qsharp.openqasm.compile(
@@ -28,16 +28,18 @@ QIR_WITH_CORRELATED_NOISE = qsharp.openqasm.compile(
 
 
 def test_noiseless_simulation():
-    output = run_qir(QIR_WITH_CORRELATED_NOISE, shots=1, noise=None)
-    assert output == [[Result.Zero, Result.One]]
+    for type in SIMULATORS:
+        output = run_qir(QIR_WITH_CORRELATED_NOISE, shots=1, noise=None, type=type)
+        assert output == [[Result.Zero, Result.One, Result.Zero]]
 
 
 def test_noisy_simulation():
     noise = NoiseConfig()
-    table = noise.intrinsic("test_noise_intrinsic", 2)
-    table.xx = 1.0
-    output = run_qir(QIR_WITH_CORRELATED_NOISE, shots=1, noise=noise)
-    assert output == [[Result.One, Result.Zero]]
+    table = noise.intrinsic("test_noise_intrinsic", 3)
+    table.yyy = 1.0
+    for type in SIMULATORS:
+        output = run_qir(QIR_WITH_CORRELATED_NOISE, shots=1, noise=noise, type=type)
+        assert output == [[Result.One, Result.Zero, Result.One]]
 
 
 def test_noisy_simulation_with_missing_gates_fails():
