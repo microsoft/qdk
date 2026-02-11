92d68787241c3d1341eba202cfcdc31ecb5b4337
Oscar Puente <oscarpuente@microsoft.com>
2026-02-11 10:33:44 -0800

    skip GPU tests if GPU is not available

--- source/pip/tests/test_correlated_noise.py
+++ source/pip/tests/test_correlated_noise.py
@@ -2,11 +2,28 @@
 # Licensed under the MIT License.
 
 import pytest
+import sys
 from qsharp._simulation import NoiseConfig, run_qir
 from qsharp import Result
 import qsharp.openqasm
 
-SIMULATORS = ("clifford", "cpu", "gpu")
+SKIP_REASON = "GPU is not available"
+
+gpu_info = "Unknown"
+
+try:
+    from qsharp._native import try_create_gpu_adapter
+
+    gpu_info = try_create_gpu_adapter()
+    # Printing to stderr so that it is visible if CI run fails
+    print(f"*** USING GPU: {gpu_info}", file=sys.stderr)
+    GPU_AVAILABLE = True
+except OSError as e:
+    GPU_AVAILABLE = False
+    SKIP_REASON = str(e)
+
+
+CPU_SIMULATORS = ("clifford", "cpu")
 QASM_WITH_CORRELATED_NOISE = """
 OPENQASM 3.0;
 include "stdgates.inc";
@@ -28,18 +45,33 @@ QIR_WITH_CORRELATED_NOISE = qsharp.openqasm.compile(
 
 
 def test_noiseless_simulation():
-    for type in SIMULATORS:
+    for type in CPU_SIMULATORS:
         output = run_qir(QIR_WITH_CORRELATED_NOISE, shots=1, noise=None, type=type)
         assert output == [[Result.Zero, Result.One, Result.Zero]]
+
+
+@pytest.mark.skipif(not GPU_AVAILABLE, reason=SKIP_REASON)
+def test_noiseless_simulation_gpu():
+    output = run_qir(QIR_WITH_CORRELATED_NOISE, shots=1, noise=None, type="gpu")
+    assert output == [[Result.Zero, Result.One, Result.Zero]]
 
 
 def test_noisy_simulation():
     noise = NoiseConfig()
     table = noise.intrinsic("test_noise_intrinsic", 3)
     table.yyy = 1.0
-    for type in SIMULATORS:
+    for type in CPU_SIMULATORS:
         output = run_qir(QIR_WITH_CORRELATED_NOISE, shots=1, noise=noise, type=type)
         assert output == [[Result.One, Result.Zero, Result.One]]
+
+
+@pytest.mark.skipif(not GPU_AVAILABLE, reason=SKIP_REASON)
+def test_noisy_simulation_gpu():
+    noise = NoiseConfig()
+    table = noise.intrinsic("test_noise_intrinsic", 3)
+    table.yyy = 1.0
+    output = run_qir(QIR_WITH_CORRELATED_NOISE, shots=1, noise=noise, type="gpu")
+    assert output == [[Result.One, Result.Zero, Result.One]]
 
 
 def test_noisy_simulation_with_missing_gates_fails():
