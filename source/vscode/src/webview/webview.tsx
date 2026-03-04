0084d645e6c2e41e92ba09f94e4b698a17434bc6
Joao Boechat <joaoboehcat@microsoft.com>
2026-03-04 10:13:12 -0800

    format command URI as JSON array

--- source/vscode/src/webview/webview.tsx
+++ source/vscode/src/webview/webview.tsx
@@ -201,11 +201,11 @@ function App({ state }: { state: State }) {
           {state.suppressSettings ? null : (
             <p style="margin-top: 8px; font-size: 0.8em">
               Note: If a{" "}
-              <a href="command:workbench.action.openSettings?%22Q%23.simulation.pauliNoise%22">
+              <a href="command:workbench.action.openSettings?%5B%22Q%23.simulation.pauliNoise%22%5D">
                 noise model
               </a>{" "}
               or any{" "}
-              <a href="command:workbench.action.openSettings?%22Q%23.simulation.qubitLoss%22">
+              <a href="command:workbench.action.openSettings?%5B%22Q%23.simulation.qubitLoss%22%5D">
                 qubit loss
               </a>{" "}
               has been configured, this may impact results
