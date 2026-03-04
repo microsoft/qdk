131458ae897e0d65947e5a43725c6b918fd941e8
Joao Boechat <joaoboehcat@microsoft.com>
2026-03-03 16:58:18 -0800

    fix error in settings reference for vscode-insiders

--- source/vscode/src/webview/webview.tsx
+++ source/vscode/src/webview/webview.tsx
@@ -201,11 +201,11 @@ function App({ state }: { state: State }) {
           {state.suppressSettings ? null : (
             <p style="margin-top: 8px; font-size: 0.8em">
               Note: If a{" "}
-              <a href="vscode://settings/Q%23.simulation.pauliNoise">
+              <a href="command:workbench.action.openSettings?%22Q%23.simulation.noiseModel%22">
                 noise model
               </a>{" "}
               or any{" "}
-              <a href="vscode://settings/Q%23.simulation.qubitLoss">
+              <a href="command:workbench.action.openSettings?%22Q%23.simulation.qubitLoss%22">
                 qubit loss
               </a>{" "}
               has been configured, this may impact results
