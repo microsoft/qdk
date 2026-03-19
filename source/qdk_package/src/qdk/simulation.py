0575c0ba7fbd3aa549a2a395eb3deb1e41071fee
Scott Carda <Scott.Carda@microsoft.com>
2026-03-19 13:26:00 -0700

    remove NeutralAtomBackend from qdk.simulators (it exists in qdk.qiskit)

--- source/qdk_package/src/qdk/simulation.py
+++ source/qdk_package/src/qdk/simulation.py
@@ -3,8 +3,3 @@
 
 from qsharp._device._atom import NeutralAtomDevice
 from qsharp._simulation import NoiseConfig
-
-try:
-    from qsharp.interop.qiskit import NeutralAtomBackend
-except ImportError:
-    pass  # qiskit extra not installed
