ca2a7a0afaf94e272ecd3f4c59c1dfabc0656607
Mine Starks <16928427+minestarks@users.noreply.github.com>
2026-03-24 20:20:22 +0000

    Set 10s test timeout for debugger test suite

--- source/vscode/test/suites/debugger/index.ts
+++ source/vscode/test/suites/debugger/index.ts
@@ -4,11 +4,14 @@
 import { runMochaTests } from "../run";
 
 export function run(): Promise<void> {
-  return runMochaTests(() => {
+  return runMochaTests(
+    () => {
     // We can't use any wildcards or dynamically discovered
     // paths here since ESBuild needs these modules to be
     // real paths on disk at bundling time.
     require("./qsharp.test"); // eslint-disable-line @typescript-eslint/no-require-imports
     require("./openqasm.test"); // eslint-disable-line @typescript-eslint/no-require-imports
-  });
+    },
+    { timeout: 10_000 },
+  );
 }
