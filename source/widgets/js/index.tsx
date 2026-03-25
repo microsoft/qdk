dc2154abbe32a53d5732812701d0cc0c64e66049
Bill Ticehurst <billti@hotmail.com>
2026-03-24 20:57:28 -0700

    Remove dataoverlay from model

--- source/widgets/js/index.tsx
+++ source/widgets/js/index.tsx
@@ -306,13 +306,11 @@ function renderMoleculeViewer({ model, el }: RenderArgs) {
     const moleculeData = model.get("molecule_data") as string;
     const cubeData = model.get("cube_data") as { [key: string]: string };
     const isoval = model.get("isoval") as number;
-    const dataOverlay = model.get("data_overlay") as { [key: string]: { [key: string]: string | number } };
     prender(
       <MoleculeViewer
         moleculeData={moleculeData}
         cubeData={cubeData || {}}
         isoValue={isoval}
-        dataOverlay={dataOverlay || {}}
       ></MoleculeViewer>,
       el,
     );
@@ -321,5 +319,4 @@ function renderMoleculeViewer({ model, el }: RenderArgs) {
   model.on("change:molecule_data", onChange);
   model.on("change:cube_data", onChange);
   model.on("change:isoval", onChange);
-  model.on("change:data_overlay", onChange);
 }
