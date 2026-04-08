642a244641ec61cb5a04eb067b172dde96a51d1c
joao-boechat <joaoboechat@microsoft.com>
2026-04-07 17:27:01 -0700

    update vscode extension to match new worker api

--- source/vscode/src/debugger/debug-service-worker.ts
+++ source/vscode/src/debugger/debug-service-worker.ts
@@ -1,10 +1,4 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-declare const __PLATFORM_DIR__: string;
-
-import { messageHandler } from "qsharp-lang/debug-service-worker";
-
-if (__PLATFORM_DIR__ === "browser") {
-  self.onmessage = messageHandler;
-}
+import "qsharp-lang/debug-service-worker";
