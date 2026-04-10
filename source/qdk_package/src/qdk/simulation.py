c5ddfce4b4621294aa67ad9990da2eeb53b039c2
Scott Carda <Scott.Carda@microsoft.com>
2026-04-10 14:42:44 -0700

    updates the `qdk` docstrings

--- source/qdk_package/src/qdk/simulation.py
+++ source/qdk_package/src/qdk/simulation.py
@@ -1,5 +1,12 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 
+"""Simulation utilities for the Q# ecosystem.
+
+Exposes :class:`~qsharp._device._atom.NeutralAtomDevice` and
+:class:`~qsharp._simulation.NoiseConfig` for configuring and running
+noise-aware quantum simulations.
+"""
+
 from qsharp._device._atom import NeutralAtomDevice
 from qsharp._simulation import NoiseConfig
