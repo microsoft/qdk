73cf37a516f67dcd23600f3370c2c7c6e04a486a
Scott Carda <Scott.Carda@microsoft.com>
2026-04-17 15:13:42 -0700

    try undoing something

--- source/pip/qsharp/interop/__init__.py
+++ source/pip/qsharp/interop/__init__.py
@@ -2,12 +2,3 @@
 # Licensed under the MIT License.
 
 """Interoperability modules for the Q# ecosystem."""
-
-from . import cirq
-
-try:
-    from . import qiskit
-
-    __all__ = ["cirq", "qiskit"]
-except ImportError:
-    __all__ = ["cirq"]
