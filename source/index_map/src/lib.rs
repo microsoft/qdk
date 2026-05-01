f839d58b0f34014ae7ceef1ff9a5a2dbe71ff6ae
Ian Davis <ian.f.davis@microsoft.com>
2026-05-01 09:48:56 -0700

    Refactor tests into their own file

--- source/index_map/src/lib.rs
+++ source/index_map/src/lib.rs
@@ -1,6 +1,9 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+#[cfg(test)]
+mod tests;
+
 use std::{
     fmt::{self, Debug, Formatter},
     iter::Enumerate,
@@ -352,36 +355,5 @@ impl IndexMut<usize> for IndexMap<usize, usize> {
     fn index_mut(&mut self, index: usize) -> &mut Self::Output {
         self.get_mut(index)
             .expect("IndexMap::index_mut: index out of bounds")
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn insert_if_absent_into_empty_returns_true() {
-        let mut map: IndexMap<usize, i32> = IndexMap::new();
-        assert!(map.insert_if_absent(0, 42));
-        assert_eq!(*map.get(0).expect("IndexMap::get: index out of bounds"), 42);
-    }
-
-    #[test]
-    fn insert_if_absent_occupied_returns_false_preserves_original() {
-        let mut map: IndexMap<usize, i32> = IndexMap::new();
-        map.insert(0, 42);
-        assert!(!map.insert_if_absent(0, 99));
-        assert_eq!(*map.get(0).expect("IndexMap::get: index out of bounds"), 42);
-    }
-
-    #[test]
-    fn insert_if_absent_extends_capacity_for_sparse_key() {
-        let mut map: IndexMap<usize, i32> = IndexMap::new();
-        assert!(map.insert_if_absent(100, 7));
-        assert_eq!(
-            *map.get(100).expect("IndexMap::get: index out of bounds"),
-            7
-        );
-        assert!(!map.contains_key(0));
     }
 }
