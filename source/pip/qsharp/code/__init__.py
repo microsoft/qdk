86fc7794a2876f1693b65c2b337bb29c35364be9
Scott Carda <Scott.Carda@microsoft.com>
2026-05-08 12:20:12 -0700

    Code proxy was overkill

--- source/pip/qsharp/code/__init__.py
+++ source/pip/qsharp/code/__init__.py
@@ -3,31 +3,10 @@
 
 # Deprecated: use qdk.code instead.
 #
-# This module proxies all attribute access to qdk.code so that dynamically
-# generated Q# callables stay in sync as content is added/removed/modified,
-# rather than taking a one-time snapshot via ``from qdk.code import *``.
+# This module replaces itself in sys.modules with the real qdk.code module
+# so that dynamically generated Q# callables stay in sync.
 
-import importlib
 import sys
-import types
-
 import qdk.code as _real_code
 
-
-class _CodeProxy(types.ModuleType):
-    """Module proxy that forwards attribute access to ``qdk.code``."""
-
-    def __getattr__(self, name):
-        return getattr(_real_code, name)
-
-    def __dir__(self):
-        return dir(_real_code)
-
-
-_proxy = _CodeProxy(__name__)
-_proxy.__package__ = __package__
-_proxy.__loader__ = __loader__  # type: ignore[name-defined]
-_proxy.__spec__ = __spec__  # type: ignore[name-defined]
-_proxy.__path__ = __path__  # type: ignore[name-defined]
-_proxy.__file__ = __file__
-sys.modules[__name__] = _proxy
+sys.modules[__name__] = _real_code
