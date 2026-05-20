9015792a9ed2e167d511e2a3cc3d0da1fc13ed4c
Dima Fedoriaka <dfedoriaka@microsoft.com>
2026-05-19 17:04:19 -0700

    implement Load/Store in sparse sim

--- source/compiler/qsc_eval/src/backend.rs
+++ source/compiler/qsc_eval/src/backend.rs
@@ -1087,7 +1087,9 @@ impl Backend for SparseSim {
             | "AccountForEstimatesInternal"
             | "BeginRepeatEstimatesInternal"
             | "EndRepeatEstimatesInternal"
-            | "EnableMemoryComputeArchitecture" => Some(Ok(Value::unit())),
+            | "EnableMemoryComputeArchitecture"
+            | "Load"
+            | "Store" => Some(Ok(Value::unit())),
             "ConfigurePauliNoise" => {
                 let [xv, yv, zv] = &*arg.unwrap_tuple() else {
                     panic!("tuple arity for ConfigurePauliNoise intrinsic should be 3");
