a9a4f00059bfff4c6a1c71b02cd551ef3cc43849
Scott Carda <Scott.Carda@microsoft.com>
2026-06-02 09:49:07 -0700

    remove unecessary re-exports

--- source/qdk_package/qdk/qre/models/factories/__init__.py
+++ source/qdk_package/qdk/qre/models/factories/__init__.py
@@ -7,15 +7,7 @@ from ._round_based import RoundBasedFactory
 from ._t_to_ccz import GSJ24CCXFactory
 from ._utils import MagicUpToClifford
 
-# Re-export types from qdk.qre that appear in signatures of classes
-# defined in this submodule so that doc-gen tools can resolve cross-references.
-from ..._qre import ISA, ISARequirements  # noqa: F401
-from ..._architecture import ISAContext  # noqa: F401
-
 __all__ = [
-    "ISA",
-    "ISAContext",
-    "ISARequirements",
     "GSJ24Factory",
     "GSJ24CCXFactory",
     "Litinski19Factory",
