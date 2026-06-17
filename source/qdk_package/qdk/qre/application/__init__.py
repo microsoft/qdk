3d656eb0e314ee75f52b7e918f3ebb1dc107dbda
Mathias Soeken <mathias.soeken@outlook.com>
2026-06-17 09:14:45 +0000

    Remove hard dependency on cirq in qdk.qre.

--- source/qdk_package/qdk/qre/application/__init__.py
+++ source/qdk_package/qdk/qre/application/__init__.py
@@ -1,15 +1,30 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 
-from ._cirq import CirqApplication, CirqApplicationParams
 from ._qir import QIRApplication
 from ._qsharp import QSharpApplication
 from ._openqasm import OpenQASMApplication
 
+try:
+    from ._cirq import CirqApplication, CirqApplicationParams
+except ImportError:
+
+    class _CirqNotInstalled:
+        """Placeholder that raises a helpful error when cirq is not installed."""
+
+        def __init__(self, *args, **kwargs):
+            raise ImportError(
+                "CirqApplication requires the 'cirq' extra. "
+                "Install it with: pip install qdk[qre,cirq]"
+            )
+
+    CirqApplication = _CirqNotInstalled
+    CirqApplicationParams = _CirqNotInstalled
+
 __all__ = [
     "CirqApplication",
     "CirqApplicationParams",
+    "OpenQASMApplication",
     "QIRApplication",
     "QSharpApplication",
-    "OpenQASMApplication",
 ]
--- source/qdk_package/qdk/qre/interop/__init__.py
+++ source/qdk_package/qdk/qre/interop/__init__.py
@@ -1,35 +1,42 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 
-from ._cirq import (
-    PeakUsageGreedyQubitManager,
-    PopBlock,
-    PushBlock,
-    QubitType,
-    ReadFromMemoryGate,
-    TypedQubit,
-    WriteToMemoryGate,
-    assert_qubits_type,
-    read_from_memory,
-    trace_from_cirq,
-    write_to_memory,
-)
 from ._qir import trace_from_qir
 from ._qsharp import trace_from_entry_expr, trace_from_entry_expr_cached
 
 __all__ = [
-    "trace_from_cirq",
     "trace_from_entry_expr",
     "trace_from_entry_expr_cached",
     "trace_from_qir",
-    "PushBlock",
-    "PopBlock",
-    "QubitType",
-    "TypedQubit",
-    "PeakUsageGreedyQubitManager",
-    "ReadFromMemoryGate",
-    "WriteToMemoryGate",
-    "write_to_memory",
-    "read_from_memory",
-    "assert_qubits_type",
 ]
+
+try:
+    from ._cirq import (
+        PeakUsageGreedyQubitManager,
+        PopBlock,
+        PushBlock,
+        QubitType,
+        ReadFromMemoryGate,
+        TypedQubit,
+        WriteToMemoryGate,
+        assert_qubits_type,
+        read_from_memory,
+        trace_from_cirq,
+        write_to_memory,
+    )
+
+    __all__ += [
+        "trace_from_cirq",
+        "PushBlock",
+        "PopBlock",
+        "QubitType",
+        "TypedQubit",
+        "PeakUsageGreedyQubitManager",
+        "ReadFromMemoryGate",
+        "WriteToMemoryGate",
+        "write_to_memory",
+        "read_from_memory",
+        "assert_qubits_type",
+    ]
+except ImportError:
+    pass
