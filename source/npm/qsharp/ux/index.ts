21c4ee0af4338d80e85f5b60130294172b1bd438
Scott Carda <Scott.Carda@microsoft.com>
2026-06-17 14:02:12 -0700

    do the same for molecule viewer

--- source/npm/qsharp/ux/index.ts
+++ source/npm/qsharp/ux/index.ts
@@ -23,7 +23,6 @@ export { EstimatesPanel } from "./estimatesPanel.js";
 export { Circuit, CircuitPanel } from "./circuit.js";
 export { setRenderer, Markdown } from "./renderers.js";
 export { Atoms, type ZoneLayout, type TraceData } from "./atoms/index.js";
-export { MoleculeViewer } from "./chem/index.js";
 export { Entanglement, type EntanglementProps } from "./entanglement.js";
 export {
   ensureTheme,
