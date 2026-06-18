b13e44366bbf7760f015a19070dac0d4e24c0445
Mathias Soeken <mathias.soeken@outlook.com>
2026-06-18 08:53:27 +0000

    No dynamic setting of `__all__`.

--- source/qdk_package/qdk/qre/interop/__init__.py
+++ source/qdk_package/qdk/qre/interop/__init__.py
@@ -4,12 +4,6 @@
 from ._qir import trace_from_qir
 from ._qsharp import trace_from_entry_expr, trace_from_entry_expr_cached
 
-__all__ = [
-    "trace_from_entry_expr",
-    "trace_from_entry_expr_cached",
-    "trace_from_qir",
-]
-
 try:
     from ._cirq import (
         PeakUsageGreedyQubitManager,
@@ -24,19 +18,47 @@ try:
         trace_from_cirq,
         write_to_memory,
     )
-
-    __all__ += [
-        "trace_from_cirq",
-        "PushBlock",
-        "PopBlock",
-        "QubitType",
-        "TypedQubit",
-        "PeakUsageGreedyQubitManager",
-        "ReadFromMemoryGate",
-        "WriteToMemoryGate",
-        "write_to_memory",
-        "read_from_memory",
-        "assert_qubits_type",
-    ]
 except ImportError:
-    pass
+    _CIRQ_INSTALL_MSG = (
+        "This function requires the 'cirq' extra. "
+        "Install it with: pip install qdk[qre,cirq]"
+    )
+
+    class _CirqNotInstalled:
+        """Placeholder that raises a helpful error when cirq is not installed."""
+
+        def __init__(self, *args, **kwargs):
+            raise ImportError(_CIRQ_INSTALL_MSG)
+
+    def _cirq_not_installed_func(*args, **kwargs):
+        """Placeholder that raises a helpful error when cirq is not installed."""
+        raise ImportError(_CIRQ_INSTALL_MSG)
+
+    PeakUsageGreedyQubitManager = _CirqNotInstalled
+    PopBlock = _CirqNotInstalled
+    PushBlock = _CirqNotInstalled
+    QubitType = _CirqNotInstalled
+    ReadFromMemoryGate = _CirqNotInstalled
+    TypedQubit = _CirqNotInstalled
+    WriteToMemoryGate = _CirqNotInstalled
+    assert_qubits_type = _cirq_not_installed_func
+    read_from_memory = _cirq_not_installed_func
+    trace_from_cirq = _cirq_not_installed_func
+    write_to_memory = _cirq_not_installed_func
+
+__all__ = [
+    "PeakUsageGreedyQubitManager",
+    "PopBlock",
+    "PushBlock",
+    "QubitType",
+    "ReadFromMemoryGate",
+    "TypedQubit",
+    "WriteToMemoryGate",
+    "assert_qubits_type",
+    "read_from_memory",
+    "trace_from_cirq",
+    "trace_from_entry_expr",
+    "trace_from_entry_expr_cached",
+    "trace_from_qir",
+    "write_to_memory",
+]
