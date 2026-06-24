159c4be0e5e2e4859523614caa29d358093e4f1a
Oscar Puente <oscarpuente@microsoft.com>
2026-06-24 13:44:06 -0700

    pretty print errors

--- source/compiler/stim_compiler/src/lib.rs
+++ source/compiler/stim_compiler/src/lib.rs
@@ -4,6 +4,7 @@
 use crate::error::Error;
 use crate::parser::parse;
 use crate::qir::compile_to_qir;
+use miette::Report;
 use qdk_simulators::noise_config::NoiseConfig;
 
 mod error;
@@ -11,10 +12,20 @@ pub mod lex;
 pub mod parser;
 pub mod qir;
 
-pub fn compile(src: &str, noise: &mut NoiseConfig<f64, f64>) -> Result<String, Vec<Error>> {
+pub fn compile(src: &str, noise: &mut NoiseConfig<f64, f64>) -> Result<String, Vec<Report>> {
     let (circuit, parser_errors) = parse(src);
     if !parser_errors.is_empty() {
-        return Err(parser_errors.into_iter().map(Error::from).collect());
+        return Err(parser_errors
+            .into_iter()
+            .map(Error::from)
+            .map(Report::new)
+            .collect());
     }
-    compile_to_qir(&circuit, noise).map_err(|errors| errors.into_iter().map(Error::from).collect())
+    compile_to_qir(&circuit, noise).map_err(|errors| {
+        errors
+            .into_iter()
+            .map(Error::from)
+            .map(Report::new)
+            .collect()
+    })
 }
--- source/compiler/stim_compiler/src/qir/tests/unsupported_instructions.rs
+++ source/compiler/stim_compiler/src/qir/tests/unsupported_instructions.rs
@@ -3,3 +3,13 @@
 
 use super::check;
 use expect_test::expect;
+
+#[test]
+fn i_error_yields_not_supported_error() {
+    let source = "I_ERROR 0";
+    check(source, &expect![[r#"
+        Stim.UnsupportedInstruction
+
+          x unsupported instruction: I_ERROR
+    "#]]);
+}
