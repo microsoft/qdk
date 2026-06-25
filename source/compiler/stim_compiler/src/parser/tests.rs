a75727f4a0eaaceb71b84a6959752b81f5c959e1
joao-boechat <joaoboechat@microsoft.com>
2026-06-25 13:57:18 -0700

    add parser instruction arguments tests

--- source/compiler/stim_compiler/src/parser/tests.rs
+++ source/compiler/stim_compiler/src/parser/tests.rs
@@ -2,26 +2,33 @@
 // Licensed under the MIT License.
 
 use expect_test::{Expect, expect};
+use miette::Report;
 
-/// Check that a stim source parses to the
-/// expected AST or yields the expected errors.
+mod arguments;
+
+/// Check that a stim source parses to the expected AST,
+/// diagnostics, or both.
 fn check(source: &str, expect: &Expect) {
     let (ast, errors) = crate::parser::parse(source);
-    if errors.is_empty() {
-        expect.assert_eq(&ast.to_string());
-    } else {
-        let buffer = errors
-            .iter()
-            .map(|e| format!("{e:?}"))
-            .collect::<Vec<_>>()
-            .join("\n");
-        expect.assert_eq(&buffer);
+    let mut entries = Vec::new();
+    if errors.is_empty() || !ast.items.is_empty() {
+        entries.push(ast.to_string());
     }
+    for error in errors {
+        entries.push(format!(
+            "{:?}",
+            Report::new(error).with_source_code(source.to_string())
+        ));
+    }
+    expect.assert_eq(&entries.join("\n"));
 }
 
 #[test]
 fn empty_src() {
-    check("", &expect![[r#"
+    check(
+        "",
+        &expect![[r#"
         Circuit [0-0]:
-            items: <empty>"#]]);
+            items: <empty>"#]],
+    );
 }
