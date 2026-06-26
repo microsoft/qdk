55eb2c2f12d809c5fe01d77faa3c8bd3cd314433
Ian Davis <Ian.F.Davis@microsoft.com>
2026-06-26 16:28:42 -0700

    Update default profile selection. (#3399)
    Updating default values of QIR profile to adaptive where appropriate.
    
    Fixes #3384

--- source/vscode/src/qirGeneration.ts
+++ source/vscode/src/qirGeneration.ts
@@ -192,7 +192,7 @@ export async function getQirForProgram(
 
 async function getQirForActiveWindowCommand() {
   try {
-    const qir = await getQirForActiveWindow("adaptive_rif", true);
+    const qir = await getQirForActiveWindow("adaptive", true);
     const qirDoc = await vscode.workspace.openTextDocument({
       language: "llvm",
       content: qir,
