c4d1a3c985ba7bbce95b1c1c1c1705932978781f
Dima Fedoriaka <dfedoriaka@microsoft.com>
2026-07-07 11:43:28 -0700

    locate library directory

--- source/qdk_package/tests-integration/test_libraries.py
+++ source/qdk_package/tests-integration/test_libraries.py
@@ -1,6 +1,11 @@
+from pathlib import Path
+
 import pytest
 from qdk import Context
 from qdk.test_utils import run_tests
+
+# Directory with all Q# libraries.
+_LIB_DIR = str(Path(__file__).resolve().parents[3] / "library")
 
 
 @pytest.mark.parametrize(
@@ -15,12 +20,12 @@ from qdk.test_utils import run_tests
     ],
 )
 def test_library(library_name: str):
-    run_tests(Context(project_root=f"library/{library_name}"))
+    run_tests(Context(project_root=f"{_LIB_DIR}/{library_name}"))
 
 
 # Use this test case for library development.
 # Run with:
 # pytest source/qdk_package/tests-integration/test_libraries.py::test_single -s
 def test_single():
-    ctx = Context(project_root="library/table_lookup")
+    ctx = Context(project_root=f"{_LIB_DIR}/table_lookup")
     run_tests(ctx, verbose=3, regex="TestLookupMatchesStd")
