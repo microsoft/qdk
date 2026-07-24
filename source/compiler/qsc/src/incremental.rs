microsoft/qdk

commit d123f370561f35188b2dfe7227735348a8b65f3e

Std.Core.GetConfig (#3486)
* Store key-value configuration map in compiler state (actually, in
PassContext).
* Add `Std.Core.GetConfig` to access config value stored inside compiler
state (actually it's stored in PassContext.
  * It must be in `Std.Core` so we can refer to it in HIR pass. 
* Add HIR transformation pass `ConfigInline` to relace`GetConfig(key,
default)` with a literal
  * If key is found, replaces call with found value.
  * Otherwise replaces with default.
  * Only supports 4 types (string, int, bool, double).
* Error if stored or default value has type other than these 4 types, or
when stored value has different type than default. This guarantees, for
example, that `GetConfig("key", 1)` will not be replaced with a String
value if config has `{'key": "1"}`.
  * Also require that both key and default value are literals.
* Add Python API to set config:
* The config must be set by passing Python dict to Context constructor.
If not passed, empty dict is used.
  * Deliberately not supported in `qsharp.init`. 
* Once `Context` is created, it's immutable. To use different config
value, new `Context` must be created. This makes it valid for
`GetConfig` to be a function, not operation.
* This is propagated from Python to Rust by passing dict to
`Interpreter::new`.
* It's impossible to mutate config after `Context` is created using
Python API.
* Tests:
  * Unit tests for rule.
* Interpreter tests (in compiler/qsc/src/interpret/tests.rs) using
simulator, including tests for all possible errors.
* One QIR generator test to make sure the GetConfig is inlined in QIR.
This single test takes 3 seconds to run. However, We we have 24 qirgen
tests, each taking 3-6 seconds to run - meaning qirgen tests are slow.

Example:
```python
import qdk
context = qdk.Context(qsharp_config={"experiment_name": "baseline", "shots": 1000})
assert context.eval('Std.Core.GetConfig("experiment_name", "")') == "baseline"
assert context.eval('Std.Core.GetConfig("shots", 100)') == 1000

source/compiler/qsc/src/incremental.rs
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-use crate::compile::{self, compile};
+use crate::compile::{self, compile_with_pass_context};
 use miette::Diagnostic;
 
 use qsc_ast::ast;
@@ -9,6 +9,7 @@ use qsc_data_structures::{
     error::WithSource, language_features::LanguageFeatures, source::SourceMap,
     target::TargetCapabilityFlags,
 };
+use qsc_eval::val::Value;
 
 use qsc_frontend::{
     compile::{Dependencies, OpenPackageStore, PackageStore},
@@ -16,6 +17,8 @@ use qsc_frontend::{
 };
 use qsc_hir::hir::PackageId;
 use qsc_passes::{PackageType, PassContext};
+use rustc_hash::FxHashMap;
+use std::rc::Rc;
 
 /// An incremental Q# compiler.
 pub struct Compiler {
@@ -45,14 +48,17 @@ impl Compiler {
         language_features: LanguageFeatures,
         mut store: PackageStore,
         dependencies: &Dependencies,
+        qsharp_config: FxHashMap<Rc<str>, Value>,
     ) -> Result<Self, Errors> {
-        let (mut unit, errors) = compile(
+        let mut passes = PassContext::new(qsharp_config);
+        let (mut unit, errors) = compile_with_pass_context(
             &store,
             dependencies,
             sources,
             package_type,
             capabilities,
             language_features,
+            &mut passes,
         );
         if !errors.is_empty() {
             return Err(errors);
@@ -77,8 +83,8 @@ impl Compiler {
         Ok(Self {
             store,
             source_package_id,
+            passes,
             frontend,
-            passes: PassContext::default(),
         })
     }
 

source/compiler/qsc/src/interpret/tests.rs
@@ -9,7 +9,8 @@ mod given_interpreter {
     use qsc_data_structures::{language_features::LanguageFeatures, target::TargetCapabilityFlags};
     use qsc_eval::{output::CursorReceiver, val::Value};
     use qsc_passes::PackageType;
-    use std::{fmt::Write, io::Cursor, iter, str::from_utf8};
+    use rustc_hash::FxHashMap;
+    use std::{fmt::Write, io::Cursor, iter, rc::Rc, str::from_utf8};
 
     fn line(interpreter: &mut Interpreter, line: &str) -> (InterpretResult, String) {
         let mut cursor = Cursor::new(Vec::<u8>::new());
@@ -93,6 +94,7 @@ mod given_interpreter {
                     LanguageFeatures::default(),
                     store,
                     &[],
+                    Default::default(),
                 )
                 .expect("interpreter should be created");
 
@@ -120,6 +122,222 @@ mod given_interpreter {
             let mut interpreter = get_interpreter();
             let (result, output) = line(&mut interpreter, "Length([1, 2, 3])");
             is_only_value(&result, &output, &Value::Int(3));
+        }
+
+        #[test]
+        fn config_value_works() {
+            let mut interpreter = get_interpreter_with_config(
+                [
+                    (Rc::from("int_config"), Value::Int(123)),
+                    (Rc::from("bool_config"), Value::Bool(true)),
+                    (Rc::from("string_config"), Value::String("value".into())),
+                    (Rc::from("double_config"), Value::Double(124.1)),
+                ]
+                .into_iter()
+                .collect(),
+            );
+
+            // Integer config.
+            let (result, output) =
+                line(&mut interpreter, "Std.Core.ConfigValue(\"int_config\", 0)");
+            is_only_value(&result, &output, &Value::Int(123));
+
+            // Boolean config.
+            let (result, output) = line(
+                &mut interpreter,
+                "Std.Core.ConfigValue(\"bool_config\", false)",
+            );
+            is_only_value(&result, &output, &Value::Bool(true));
+
+            // String config.
+            let (result, output) = line(
+                &mut interpreter,
+                "Std.Core.ConfigValue(\"string_config\", \"\")",
+            );
+            is_only_value(&result, &output, &Value::String("value".into()));
+
+            // Double config.
+            let (result, output) = line(
+                &mut interpreter,
+                "Std.Core.ConfigValue(\"double_config\", 0.0)",
+            );
+            is_only_value(&result, &output, &Value::Double(124.1));
+
+            // Default value.
+            let (result, output) = line(&mut interpreter, "Std.Core.ConfigValue(\"unknown\", 15)");
+            is_only_value(&result, &output, &Value::Int(15));
+
+            // ConfigValue can be used as an argument to another function.
+            let (result, output) = line(
+                &mut interpreter,
+                "Std.Math.MaxI(1, Std.Core.ConfigValue(\"int_config\", 0))",
+            );
+            is_only_value(&result, &output, &Value::Int(123));
+        }
+
+        #[test]
+        fn config_value_works_with_sources() {
+            let source = indoc! { r#"
+            namespace OptimizationConfig {
+                function IsMinimizingQubits() : Bool {
+                    Std.Core.ConfigValue("minimize_qubits", true)
+                }
+            }
+
+            namespace Test {
+                @EntryPoint()
+                operation Main() : Bool {
+                    OptimizationConfig.IsMinimizingQubits()
+                }
+            }
+            "#};
+
+            let sources =
+                SourceMap::new([("test".into(), source.into())], Some("Test.Main()".into()));
+            let (std_id, store) =
+                crate::compile::package_store_with_stdlib(TargetCapabilityFlags::all());
+            let mut interpreter = Interpreter::new(
+                sources,
+                PackageType::Exe,
+                TargetCapabilityFlags::all(),
+                LanguageFeatures::default(),
+                store,
+                &[(std_id, None)],
+                [(Rc::from("minimize_qubits"), Value::Bool(false))]
+                    .into_iter()
+                    .collect(),
+            )
+            .expect("interpreter should be created");
+
+            let (result, output) = entry(&mut interpreter);
+            is_only_value(&result, &output, &Value::Bool(false));
+        }
+
+        #[test]
+        #[allow(clippy::too_many_lines)]
+        fn config_value_errors() {
+            let mut interpreter = get_interpreter_with_config(
+                [
+                    (Rc::from("int_config"), Value::Int(123)),
+                    (
+                        Rc::from("result_config"),
+                        Value::Result(qsc_eval::val::Result::Loss),
+                    ),
+                ]
+                .into_iter()
+                .collect(),
+            );
+            // Error when default type doesn't match stored config value.
+            let (result, output) = line(
+                &mut interpreter,
+                "Std.Core.ConfigValue(\"int_config\", 20.0)",
+            );
+            is_only_error(
+                &result,
+                &output,
+                &expect![[r#"
+                    configuration value type does not match ConfigValue default value type
+                       [line_0] [20.0]
+                "#]],
+            );
+
+            // Error when key is not literal.
+            let (result, output) = line(
+                &mut interpreter,
+                "Std.Core.ConfigValue(\"int_\" + \"config\", 20)",
+            );
+            is_only_error(
+                &result,
+                &output,
+                &expect![[r#"
+                    ConfigValue arguments must be literals
+                       [line_1] ["int_" + "config"]
+                "#]],
+            );
+
+            // Error when value is not literal.
+            let (result, output) = line(
+                &mut interpreter,
+                "Std.Core.ConfigValue(\"int_config\", 10+10)",
+            );
+            is_only_error(
+                &result,
+                &output,
+                &expect![[r#"
+                    ConfigValue arguments must be literals
+                       [line_2] [10+10]
+                "#]],
+            );
+
+            // Error when value is a variable.
+            let (result, output) = line(
+                &mut interpreter,
+                "let default=10; Std.Core.ConfigValue(\"int_config\", default)",
+            );
+            is_only_error(
+                &result,
+                &output,
+                &expect![[r#"
+                    ConfigValue arguments must be literals
+                       [line_3] [default]
+                "#]],
+            );
+
+            // Error when config contains value of unsupported type (same as default type).
+            let (result, output) = line(
+                &mut interpreter,
+                "Std.Core.ConfigValue(\"result_config\", Zero)",
+            );
+            is_only_error(
+                &result,
+                &output,
+                &expect![[r#"
+                    unsupported configuration type
+                       [line_4] [Zero]
+                "#]],
+            );
+
+            // Error when config contains value of unsupported type (defferent than default type)
+            let (result, output) = line(
+                &mut interpreter,
+                "Std.Core.ConfigValue(\"result_config\", 0.5)",
+            );
+            is_only_error(
+                &result,
+                &output,
+                &expect![[r#"
+                    unsupported configuration type
+                       [line_5] [0.5]
+                "#]],
+            );
+
+            // Error when config is missing and default type is unsupported.
+            let (result, output) = line(
+                &mut interpreter,
+                "Std.Core.ConfigValue(\"bigint_config\", 10L)",
+            );
+            is_only_error(
+                &result,
+                &output,
+                &expect![[r#"
+                    unsupported configuration type
+                       [line_6] [10L]
+                "#]],
+            );
+
+            // Error when using ConfigValue not in a call.
+            let (result, output) = line(
+                &mut interpreter,
+                "let f = Std.Core.ConfigValue; f(\"key\", 5)",
+            );
+            is_only_error(
+                &result,
+                &output,
+                &expect![[r#"
+                    ConfigValue must be called directly
+                       [line_7] [Std.Core.ConfigValue]
+                "#]],
+            );
         }
 
         #[test]
@@ -1564,6 +1782,75 @@ mod given_interpreter {
         }
 
         #[test]
+        fn qirgen_folds_get_config_values() {
+            let source = indoc! {"
+                operation Main() : Unit {
+                    use q = Qubit[3];
+                    Rx(Std.Core.ConfigValue(\"angle1\", 0.1), q[0]);
+                    Ry(Std.Core.ConfigValue(\"angle2\", 0.2), q[1]);
+                    let loop_iterations = Std.Core.ConfigValue(\"loop_iterations\", 0);
+                    for i in 1..loop_iterations {
+                        X(q[2]);
+                    }
+                }
+            "};
+
+            let mut interpreter = get_interpreter_with_capabilities_and_config(
+                TargetCapabilityFlags::empty(),
+                [
+                    (Rc::from("angle1"), Value::Double(0.6)),
+                    (Rc::from("loop_iterations"), Value::Int(3)),
+                ]
+                .into_iter()
+                .collect(),
+            );
+            _ = line(&mut interpreter, source);
+
+            let qir = interpreter.qirgen("Main()").expect("expected success");
+            expect![[r#"
+                %Result = type opaque
+                %Qubit = type opaque
+
+                @0 = internal constant [4 x i8] c"0_t\00"
+
+                define i64 @ENTRYPOINT__main() #0 {
+                block_0:
+                  call void @__quantum__rt__initialize(i8* null)
+                  call void @__quantum__qis__rx__body(double 0.6, %Qubit* inttoptr (i64 0 to %Qubit*))
+                  call void @__quantum__qis__ry__body(double 0.2, %Qubit* inttoptr (i64 1 to %Qubit*))
+                  call void @__quantum__qis__x__body(%Qubit* inttoptr (i64 2 to %Qubit*))
+                  call void @__quantum__qis__x__body(%Qubit* inttoptr (i64 2 to %Qubit*))
+                  call void @__quantum__qis__x__body(%Qubit* inttoptr (i64 2 to %Qubit*))
+                  call void @__quantum__rt__tuple_record_output(i64 0, i8* getelementptr inbounds ([4 x i8], [4 x i8]* @0, i64 0, i64 0))
+                  ret i64 0
+                }
+
+                declare void @__quantum__rt__initialize(i8*)
+
+                declare void @__quantum__qis__rx__body(double, %Qubit*)
+
+                declare void @__quantum__qis__ry__body(double, %Qubit*)
+
+                declare void @__quantum__qis__x__body(%Qubit*)
+
+                declare void @__quantum__rt__tuple_record_output(i64, i8*)
+
+                attributes #0 = { "entry_point" "output_labeling_schema" "qir_profiles"="base_profile" "required_num_qubits"="3" "required_num_results"="0" }
+                attributes #1 = { "irreversible" }
+
+                ; module flags
+
+                !llvm.module.flags = !{!0, !1, !2, !3}
+
+                !0 = !{i32 1, !"qir_major_version", i32 1}
+                !1 = !{i32 7, !"qir_minor_version", i32 0}
+                !2 = !{i32 1, !"dynamic_qubit_management", i1 false}
+                !3 = !{i32 1, !"dynamic_result_management", i1 false}
+            "#]]
+            .assert_eq(&qir);
+        }
+
+        #[test]
         fn adaptive_qirgen() {
             let mut interpreter = get_interpreter_with_capabilities(
                 TargetCapabilityFlags::Adaptive | TargetCapabilityFlags::IntegerComputations,
@@ -2301,21 +2588,24 @@ mod given_interpreter {
     }
 
     fn get_interpreter() -> Interpreter {
-        let (std_id, store) =
-            crate::compile::package_store_with_stdlib(TargetCapabilityFlags::all());
-        let dependencies = &[(std_id, None)];
-        Interpreter::new(
-            SourceMap::default(),
-            PackageType::Lib,
+        get_interpreter_with_capabilities_and_config(
             TargetCapabilityFlags::all(),
-            LanguageFeatures::default(),
-            store,
-            dependencies,
+            FxHashMap::default(),
         )
-        .expect("interpreter should be created")
+    }
+
+    fn get_interpreter_with_config(qsharp_config: FxHashMap<Rc<str>, Value>) -> Interpreter {
+        get_interpreter_with_capabilities_and_config(TargetCapabilityFlags::all(), qsharp_config)
     }
 
     fn get_interpreter_with_capabilities(capabilities: TargetCapabilityFlags) -> Interpreter {
+        get_interpreter_with_capabilities_and_config(capabilities, FxHashMap::default())
+    }
+
+    fn get_interpreter_with_capabilities_and_config(
+        capabilities: TargetCapabilityFlags,
+        qsharp_config: FxHashMap<Rc<str>, Value>,
+    ) -> Interpreter {
         let (std_id, store) = crate::compile::package_store_with_stdlib(capabilities);
         let dependencies = &[(std_id, None)];
         Interpreter::new(
@@ -2325,6 +2615,7 @@ mod given_interpreter {
             LanguageFeatures::default(),
             store,
             dependencies,
+            qsharp_config,
         )
         .expect("interpreter should be created")
     }
@@ -2441,6 +2732,7 @@ mod given_interpreter {
                 LanguageFeatures::default(),
                 store,
                 &[(std_id, None)],
+                Default::default(),
             )
             .expect("interpreter should be created");
 
@@ -2463,6 +2755,7 @@ mod given_interpreter {
                     LanguageFeatures::default(),
                     store,
                     &[(std_id, None)],
+                    Default::default(),
                 )
                 .is_err(),
                 "interpreter should fail with error"
@@ -2484,6 +2777,7 @@ mod given_interpreter {
                 LanguageFeatures::default(),
                 store,
                 &[(std_id, None)],
+                Default::default(),
             );
 
             match result {
@@ -2539,6 +2833,7 @@ mod given_interpreter {
                 LanguageFeatures::default(),
                 store,
                 &[(std_id, None)],
+                Default::default(),
             )
             .expect("interpreter should be created without a false-positive capability rejection");
 
@@ -2573,6 +2868,7 @@ mod given_interpreter {
                 LanguageFeatures::default(),
                 store,
                 &[(std_id, None)],
+                Default::default(),
             );
 
             match result {
@@ -2627,6 +2923,7 @@ mod given_interpreter {
                 LanguageFeatures::default(),
                 store,
                 &[(std_id, None)],
+                Default::default(),
             );
 
             match result {
@@ -2675,6 +2972,7 @@ mod given_interpreter {
                 LanguageFeatures::default(),
                 store,
                 &[(std_id, None)],
+                Default::default(),
             );
 
             assert!(
@@ -2720,6 +3018,7 @@ mod given_interpreter {
                 LanguageFeatures::default(),
                 store,
                 &[(std_id, None)],
+                Default::default(),
             );
 
             match result {
@@ -2763,6 +3062,7 @@ mod given_interpreter {
                 LanguageFeatures::default(),
                 store,
                 dependencies,
+                Default::default(),
             )
             .expect("interpreter should be created");
 
@@ -2792,6 +3092,7 @@ mod given_interpreter {
                 LanguageFeatures::default(),
                 store,
                 &[],
+                Default::default(),
             )
             .expect("interpreter should be created");
 
@@ -2976,6 +3277,7 @@ mod given_interpreter {
                 LanguageFeatures::default(),
                 store,
                 &[],
+                Default::default(),
             )
             .expect("interpreter should be created");
             let (result, output) = line(&mut interpreter, "Test.Hello()");
@@ -3009,6 +3311,7 @@ mod given_interpreter {
                 LanguageFeatures::default(),
                 store,
                 &[],
+                Default::default(),
             )
             .expect("interpreter should be created");
 
@@ -3046,6 +3349,7 @@ mod given_interpreter {
                 LanguageFeatures::default(),
                 store,
                 &[(std_id, None)],
+                Default::default(),
             )
             .expect("interpreter should be created");
 
@@ -3157,6 +3461,7 @@ mod given_interpreter {
                 LanguageFeatures::default(),
                 store,
                 &[(std_id, None)],
+                Default::default(),
             )
             .expect("interpreter should be created");
 
@@ -3193,6 +3498,7 @@ mod given_interpreter {
                 LanguageFeatures::default(),
                 store,
                 &[(std_id, None)],
+                Default::default(),
             )
             .expect("interpreter should be created");
 
@@ -3272,6 +3578,7 @@ mod given_interpreter {
                 LanguageFeatures::default(),
                 store,
                 &[(std_id, None)],
+                Default::default(),
             )
             .expect("interpreter should be created");
 
@@ -3306,6 +3613,7 @@ mod given_interpreter {
                 LanguageFeatures::default(),
                 store,
                 &[(std_id, None)],
+                Default::default(),
             )
             .expect("interpreter should be created");
 
@@ -3339,6 +3647,7 @@ mod given_interpreter {
                 LanguageFeatures::default(),
                 store,
                 &[(std_id, None)],
+                Default::default(),
             ) {
                 Ok(_) => panic!("interpreter should fail with error"),
                 Err(errors) => {

source/compiler/qsc_passes/src/lib.rs
@@ -5,6 +5,7 @@ mod borrowck;
 mod callable_limits;
 mod capabilitiesck;
 mod common;
+mod config_inline;
 mod conjugate_invert;
 mod entry_point;
 mod id_update;
@@ -29,6 +30,7 @@ use index_assignment::ConvertToWSlash;
 use loop_unification::LoopUni;
 use miette::Diagnostic;
 use qsc_data_structures::target::TargetCapabilityFlags;
+use qsc_eval::val::Value;
 use qsc_fir::fir;
 use qsc_frontend::compile::CompileUnit;
 use qsc_hir::{
@@ -42,6 +44,8 @@ use qsc_hir::{
 use qsc_lowerer::map_hir_package_to_fir;
 use qsc_rca::{PackageComputeProperties, PackageStoreComputeProperties};
 use replace_qubit_allocation::ReplaceQubitAllocation;
+use rustc_hash::FxHashMap;
+use std::rc::Rc;
 use thiserror::Error;
 
 pub(crate) static CORE_NAMESPACE: &[&str] = &["Std", "Core"];
@@ -54,6 +58,7 @@ pub enum Error {
     BorrowCk(borrowck::Error),
     CallableLimits(callable_limits::Error),
     CapabilitiesCk(qsc_rca::errors::Error),
+    ConfigInline(config_inline::Error),
     ConjInvert(conjugate_invert::Error),
     EntryPoint(entry_point::Error),
     Measurement(measurement::Error),
@@ -85,19 +90,22 @@ pub fn lower_hir_to_fir(
 
 pub struct PassContext {
     borrow_check: borrowck::Checker,
+    /// Read-only config values exposed to Q# via Std.Core.ConfigValue.
+    qsharp_config: FxHashMap<Rc<str>, Value>,
 }
 
 impl Default for PassContext {
     fn default() -> Self {
-        Self::new()
+        Self::new(FxHashMap::default())
     }
 }
 
 impl PassContext {
     #[must_use]
-    pub fn new() -> Self {
+    pub fn new(qsharp_config: FxHashMap<Rc<str>, Value>) -> Self {
         Self {
             borrow_check: borrowck::Checker::default(),
+            qsharp_config,
         }
     }
 
@@ -125,6 +133,9 @@ impl PassContext {
         let conjugate_errors = conjugate_invert::invert_conjugate_exprs(core, package, assigner);
         Validator::default().visit_package(package);
 
+        let config_inline_errors =
+            config_inline::replace_get_config_calls(core, package, &self.qsharp_config);
+
         let measurement_decl_errors = measurement::validate_measurement_declarations(package);
         let reset_decl_errors = reset::validate_reset_declarations(package);
         let noise_intrinsic_decl_errors =
@@ -149,6 +160,7 @@ impl PassContext {
             .chain(spec_errors.into_iter().map(Error::SpecGen))
             .chain(conjugate_errors.into_iter().map(Error::ConjInvert))
             .chain(entry_point_errors)
+            .chain(config_inline_errors.into_iter().map(Error::ConfigInline))
             .chain(measurement_decl_errors.into_iter().map(Error::Measurement))
             .chain(reset_decl_errors.into_iter().map(Error::Reset))
             .chain(
@@ -175,7 +187,12 @@ pub fn run_default_passes(
     unit: &mut CompileUnit,
     package_type: PackageType,
 ) -> Vec<Error> {
-    PassContext::new().run_default_passes(&mut unit.package, &mut unit.assigner, core, package_type)
+    PassContext::default().run_default_passes(
+        &mut unit.package,
+        &mut unit.assigner,
+        core,
+        package_type,
+    )
 }
 
 pub fn run_core_passes(core: &mut CompileUnit) -> Vec<Error> {

commit 51f714b91368355ef9ca974f947d9099facb7c08

Fix a bug in Compiler::new (#3509)
* There was a bug introduced in
https://github.com/microsoft/qdk/pull/3486 when instead of using two
separate instances of PassContext (one to compile sources and one to
keep in Compiler for incremental compilation), it used the same
PassContext.
* It manifested itself as false-positive "lambdas cannot close over
mutable variables" compilation error, but the root cause was that
Assigner and borrowck::Checker were having different lifetimes. So if
the first pass had a mutable variable, its NodeId would be stored in the
set of mutable variables. On second pass if new assigner happens to
assign same NodeId to immutable variable and that variable is used in
lambda, that would trigger an error.
* This PR restores having 2 PassContexts, as it was prior to
https://github.com/microsoft/qdk/pull/3486 and adds a regression test.
* To avoid cloning the map, I changed stored type of config to be
`Rc<FxHashMap>`.

source/compiler/qsc/src/incremental.rs
@@ -50,7 +50,8 @@ impl Compiler {
         dependencies: &Dependencies,
         qsharp_config: FxHashMap<Rc<str>, Value>,
     ) -> Result<Self, Errors> {
-        let mut passes = PassContext::new(qsharp_config);
+        let qsharp_config_rc = Rc::new(qsharp_config);
+        let mut passes = PassContext::new(qsharp_config_rc.clone());
         let (mut unit, errors) = compile_with_pass_context(
             &store,
             dependencies,
@@ -83,7 +84,7 @@ impl Compiler {
         Ok(Self {
             store,
             source_package_id,
-            passes,
+            passes: PassContext::new(qsharp_config_rc),
             frontend,
         })
     }

source/compiler/qsc/src/interpret/tests.rs
@@ -3667,5 +3667,48 @@ mod given_interpreter {
                 }
             }
         }
+
+        /// Regression test for `MutableClosure` false positive compilation error which can happen
+        /// if the same `PassContext` is reused to compile sources and for interactive compilation.
+        #[test]
+        fn valid_lambda_in_second_compilation_should_not_error() {
+            let source = String::from(
+                r#"
+                    namespace Test {
+                        function Add(a : Int, b : Int) : Int { a + b }
+                        operation Seed() : Int {mutable m1 = 1; 0 }
+                    }
+                "#,
+            );
+
+            let sources = SourceMap::new([("test".into(), source.into())], None);
+            let (std_id, store) =
+                crate::compile::package_store_with_stdlib(TargetCapabilityFlags::all());
+            let mut interpreter = Interpreter::new(
+                sources,
+                PackageType::Lib,
+                TargetCapabilityFlags::all(),
+                LanguageFeatures::default(),
+                store,
+                &[(std_id, None)],
+                Default::default(),
+            )
+            .expect("interpreter should be created");
+
+            let (result, output) = line(
+                &mut interpreter,
+                indoc! {r#"
+                    operation Run() : Int {
+                        let x = 1;
+                        let f = r -> Test.Add(x, _)(r[0]);
+                        f([1])
+                    }
+                "#},
+            );
+            is_only_value(&result, &output, &Value::unit());
+
+            let (result, output) = line(&mut interpreter, "Run()");
+            is_only_value(&result, &output, &Value::Int(2));
+        }
     }
 }

source/compiler/qsc_passes/src/lib.rs
@@ -91,18 +91,18 @@ pub fn lower_hir_to_fir(
 pub struct PassContext {
     borrow_check: borrowck::Checker,
     /// Read-only config values exposed to Q# via Std.Core.ConfigValue.
-    qsharp_config: FxHashMap<Rc<str>, Value>,
+    qsharp_config: Rc<FxHashMap<Rc<str>, Value>>,
 }
 
 impl Default for PassContext {
     fn default() -> Self {
-        Self::new(FxHashMap::default())
+        Self::new(Default::default())
     }
 }
 
 impl PassContext {
     #[must_use]
-    pub fn new(qsharp_config: FxHashMap<Rc<str>, Value>) -> Self {
+    pub fn new(qsharp_config: Rc<FxHashMap<Rc<str>, Value>>) -> Self {
         Self {
             borrow_check: borrowck::Checker::default(),
             qsharp_config,