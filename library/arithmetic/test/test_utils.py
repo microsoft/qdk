510a85d2c3c99a703f59b20abf876fc49ca01645
Dima Fedoriaka <dfedoriaka@microsoft.com>
2026-07-27 13:45:05 -0700

    prettier fix

--- library/arithmetic/test/test_utils.py
+++ library/arithmetic/test/test_utils.py
@@ -6,7 +6,7 @@ from qdk import Context
 
 @functools.cache
 def get_qdk_context(*, optimize: str = "") -> Context:
-    # Context is cached to avoid re-compiling the library for each
+    # Context is cached to avoid re-compiling the library for each test.
     path = str(Path(__file__).resolve().parents[1])
     return Context(
         project_root=path, qdk_config={"optimize": optimize}
