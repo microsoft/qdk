f4e26417129ba8f803d4048bc9652cb7d6d7b27d
Dima Fedoriaka <dfedoriaka@microsoft.com>
2026-07-30 17:08:01 -0700

    patch dependencies in library tests

--- source/qdk_package/tests-integration/test_libraries.py
+++ source/qdk_package/tests-integration/test_libraries.py
@@ -1,3 +1,5 @@
+import json
+import os
 from pathlib import Path
 
 import pytest
@@ -5,7 +7,43 @@ from qdk import Context
 from qdk.test_utils import run_tests
 
 # Directory with all Q# libraries.
-_LIB_DIR = str(Path(__file__).resolve().parents[3] / "library")
+_LIB_PATH = Path(__file__).resolve().parents[3] / "library"
+
+
+def patch_dependencies(manifest_path: Path) -> None:
+    """Patches dependencies in given Q# manifest to depend on local version of QDK 
+    libraries instead of the ones on GitHub.
+
+    Applies only to libraries in microsoft/qdk/library.
+    """
+    manifest = json.loads(manifest_path.read_text(encoding="utf-8"))
+    updated = False
+
+    for dependency in manifest.get("dependencies", {}).values():
+        github = dependency.get("github")
+        if not isinstance(github, dict):
+            continue
+
+        owner = github.get("owner")
+        repo = github.get("repo")
+        library_path = github.get("path")
+        if (
+            isinstance(owner, str)
+            and owner.lower() == "microsoft"
+            and isinstance(repo, str)
+            and repo.lower() == "qdk"
+            and isinstance(library_path, str)
+        ):
+            library_name = Path(library_path).name
+            dependency.clear()
+            dependency["path"] = str(_LIB_PATH / library_name)
+            updated = True
+            print(f"AAAA patched dep {library_name} in {manifest_path}")
+
+    if updated:
+        manifest_path.write_text(
+            json.dumps(manifest, indent=2) + "\n", encoding="utf-8"
+        )
 
 
 @pytest.mark.parametrize(
@@ -20,12 +58,14 @@ _LIB_DIR = str(Path(__file__).resolve().parents[3] / "library")
     ],
 )
 def test_library(library_name: str):
-    run_tests(context=Context(project_root=f"{_LIB_DIR}/{library_name}"))
+    if os.environ.get("CI") is not None:
+        patch_dependencies(_LIB_PATH / library_name / "qsharp.json")
+    run_tests(context=Context(project_root=f"{_LIB_PATH}/{library_name}"))
 
 
 # Use this test case for library development.
 # Run with:
 # pytest source/qdk_package/tests-integration/test_libraries.py::test_single -s
 def test_single():
-    ctx = Context(project_root=f"{_LIB_DIR}/table_lookup")
+    ctx = Context(project_root=f"{_LIB_PATH}/table_lookup")
     run_tests(context=ctx, verbose=3, regex="TestLookupMatchesStd")
