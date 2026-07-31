c80df9cafc4d3c826f366afa245d47510a4b7379
Dima Fedoriaka <dfedoriaka@microsoft.com>
2026-07-31 12:24:50 -0700

    add -s to see output in CI

--- build.py
+++ build.py
@@ -444,7 +444,7 @@ def run_python_integration_tests(cwd, interpreter):
         cwd,
         os.path.join(root_dir, "library", "arithmetic", "test"),
     ]
-    command_args = [interpreter, "-m", "pytest", *paths]
+    command_args = [interpreter, "-m", "pytest", *paths, "-s"]
     subprocess.run(command_args, check=True, text=True, cwd=cwd)
 
 
