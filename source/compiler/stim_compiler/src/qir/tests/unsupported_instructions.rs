3c23cdeac4c244ac8a7abaeb3f7f9e245a68fa29
joao-boechat <joaoboechat@microsoft.com>
2026-08-07 01:46:53 -0700

    update tests for mpp

--- source/compiler/stim_compiler/src/qir/tests/unsupported_instructions.rs
+++ source/compiler/stim_compiler/src/qir/tests/unsupported_instructions.rs
@@ -40,66 +40,6 @@ fn heralded_pauli_channel_1_yields_unsupported_error() {
 }
 
 #[test]
-fn mpp_yields_unsupported_error() {
-    let source = indoc! {"
-        # Measure the two-body +X1*Y2 observable.
-        MPP X1*Y2
-
-        # Measure the one-body -Z5 observable.
-        MPP !Z5
-
-        # Measure the two-body +X1*Y2 observable and also the three-body -Z3*Z4*Z5 observable.
-        MPP X1*Y2 !Z3*Z4*Z5
-
-        # Noisily measure +Z1+Z2 and +X1*X2 (independently flip each reported result 0.1% of the time).
-        MPP(0.001) Z1*Z2 X1*X2
-    "};
-    check(
-        source,
-        &expect![[r#"
-            Qdk.Stim.Compiler.UnsupportedInstruction
-
-              x unsupported instruction: MPP
-               ,-[2:1]
-             1 | # Measure the two-body +X1*Y2 observable.
-             2 | MPP X1*Y2
-               : ^^^^^^^^^
-             3 | 
-               `----
-
-            Qdk.Stim.Compiler.UnsupportedInstruction
-
-              x unsupported instruction: MPP
-               ,-[5:1]
-             4 | # Measure the one-body -Z5 observable.
-             5 | MPP !Z5
-               : ^^^^^^^
-             6 | 
-               `----
-
-            Qdk.Stim.Compiler.UnsupportedInstruction
-
-              x unsupported instruction: MPP
-               ,-[8:1]
-             7 | # Measure the two-body +X1*Y2 observable and also the three-body -Z3*Z4*Z5 observable.
-             8 | MPP X1*Y2 !Z3*Z4*Z5
-               : ^^^^^^^^^^^^^^^^^^^
-             9 | 
-               `----
-
-            Qdk.Stim.Compiler.UnsupportedInstruction
-
-              x unsupported instruction: MPP
-                ,-[11:1]
-             10 | # Noisily measure +Z1+Z2 and +X1*X2 (independently flip each reported result 0.1% of the time).
-             11 | MPP(0.001) Z1*Z2 X1*X2
-                : ^^^^^^^^^^^^^^^^^^^^^^
-                `----
-        "#]],
-    );
-}
-
-#[test]
 fn spp_yields_unsupported_error() {
     let source = indoc! {"
         # Perform an S gate on qubit 1.
