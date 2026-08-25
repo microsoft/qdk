97f6ac3d17cd13707d40bc7957f6e19a58bb891c
joao-boechat <joaoboechat@microsoft.com>
2026-08-25 14:55:56 -0700

    fix type issue in test

--- source/qdk_package/tests/test_stim.py
+++ source/qdk_package/tests/test_stim.py
@@ -1,15 +1,35 @@
 import math
 
 import qdk.stim as stim
+from qdk import Result
 
 from simulator_test_utils import check_histogram
+
+
+def map_result_list_to_str(results):
+    s = ""
+    if isinstance(results, (list, tuple)):
+        for r in results:
+            s += map_result_list_to_str(r)
+    else:
+        match results:
+            case Result.Zero:
+                s += "0"
+            case Result.One:
+                s += "1"
+            case Result.Loss:
+                s += "L"
+    return s
 
 
 def test_t_gate_runs_on_clifford_simulator() -> None:
     results = stim.run("H 0\nT 0\nH 0\nM 0", shots=2_000, seed=42, type="clifford")
 
     one_probability = math.sin(math.pi / 8.0) ** 2
-    check_histogram(results, {"0": 1.0 - one_probability, "1": one_probability})
+    check_histogram(
+        [map_result_list_to_str(result) for result in results],
+        {"0": 1.0 - one_probability, "1": one_probability},
+    )
 
 
 def test_arbitrary_rotation_runs_on_clifford_simulator() -> None:
@@ -19,4 +39,7 @@ def test_arbitrary_rotation_runs_on_clifford_simulator() -> None:
     )
 
     one_probability = math.sin(math.pi * half_turns / 2.0) ** 2
-    check_histogram(results, {"0": 1.0 - one_probability, "1": one_probability})
+    check_histogram(
+        [map_result_list_to_str(result) for result in results],
+        {"0": 1.0 - one_probability, "1": one_probability},
+    )
