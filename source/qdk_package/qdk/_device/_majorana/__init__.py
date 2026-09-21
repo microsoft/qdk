04dbaf370ab3df5ec73774a57a18b0e28c559794
Stefan J. Wernli <swernli@microsoft.com>
2026-09-20 23:20:24 -0700

    Add virtual device

--- source/qdk_package/qdk/_device/_majorana/__init__.py
+++ source/qdk_package/qdk/_device/_majorana/__init__.py
@@ -7,8 +7,8 @@ from typing import Tuple, Union, Callable, Any, Optional, Literal, List, Generat
 
 
 class MajoranaDevice:
-    def __init__(self, layout: Tuple[int, int]):
-        self.layout = layout
+    def __init__(self, rows: int, cols: int):
+        self.layout = (rows, cols)
 
     def run(
         self,
@@ -18,6 +18,7 @@ class MajoranaDevice:
         type: Optional[Literal["sparse", "clifford"]] = None,
         num_qubits: Optional[int] = None,
         show_trace: bool = True,
+        use_virtual: bool = False,
     ):
         from IPython.display import display  # type: ignore[import-not-found]
 
@@ -42,16 +43,18 @@ class MajoranaDevice:
                     "The qsharp-widgets package is required for showing Majorana trace visualization. "
                     "Please install it via 'pip install \"qdk[jupyter]\"' or 'pip install qsharp-widgets'."
                 )
-
-            trace = schedule(
-                self.layout,
-                eval(result["trace"]),
-            )
+            if use_virtual:
+                trace = schedule_virtual(self.layout, eval(result["trace"]))
+            else:
+                trace = schedule_tetrons(
+                    self.layout,
+                    eval(result["trace"]),
+                )
             display(Majorana(self.layout, trace))
         return result["result"]
 
 
-def schedule(
+def schedule_tetrons(
     layout: Tuple[int, int], trace: List[Tuple[str, List[int]]]
 ) -> List[List[Tuple[str, List[int]]]]:
     scheduled_trace = []
@@ -60,7 +63,7 @@ def schedule(
         match event[0]:
             case "X" | "Y" | "Z":
                 continue
-        if event[0].endswith("-lw") and event[1][0] in bottom_row_ids(layout):
+        if event[0].endswith("-lw") and event[1][0] in tetron_bottom_row_ids(layout):
             event = (event[0].replace("-lw", "-up"), event[1])
 
         # Find the last step in the scheduled trace that includes this event's qubits,
@@ -87,7 +90,7 @@ def schedule(
                     if step_idx >= len(scheduled_trace):
                         # Couldn't find a place for this lower wire operation, but check
                         # to see if there is a better spot for an upper wire operation on the same qubit.
-                        if event[1][0] not in top_row_ids(layout):
+                        if event[1][0] not in tetron_top_row_ids(layout):
                             neighbor = event[1][0] - 2
                             upper_step = step_idx
                             while any(
@@ -105,10 +108,10 @@ def schedule(
                                     event[0].replace("-lw", "-up"),
                                     event[1],
                                 )
-                            else:
-                                # Couldn't find a better spot for the upper wire operation, so
-                                # put the lower wire operation at the end.
-                                scheduled_trace.append([])
+                                break
+                        # Couldn't find a better spot for the upper wire operation, so
+                        # put the lower wire operation at the end.
+                        scheduled_trace.append([])
                         break
             elif event[0].endswith("-up"):
                 # If the event is an upper wire operation, ensure its top neighbor
@@ -142,23 +145,127 @@ def convert(event: Tuple[str, List[int]]) -> Tuple[str, List[int]]:
             if targets[0] > targets[1]:
                 name = "Mzy"
                 targets = [targets[1], targets[0]]
+        case "Mzy":
+            if targets[0] > targets[1]:
+                name = "Myz"
+                targets = [targets[1], targets[0]]
         case "Mxx":
+            if targets[0] > targets[1]:
+                targets = [targets[1], targets[0]]
+        case "Mzz" | "Myy":
             if targets[0] > targets[1]:
                 targets = [targets[1], targets[0]]
     return (name, targets)
 
 
-def bottom_row_ids(layout: Tuple[int, int]) -> Generator[int]:
+def tetron_bottom_row_ids(layout: Tuple[int, int]) -> Generator[int]:
     rows, cols = layout
     for col in range(cols):
         idx = (col + 1) * (rows * 4) - 1
-        yield idx
         yield idx - 1
+        yield idx
 
 
-def top_row_ids(layout: Tuple[int, int]) -> Generator[int]:
+def tetron_top_row_ids(layout: Tuple[int, int]) -> Generator[int]:
     rows, cols = layout
     for col in range(cols):
         idx = col * (rows * 4)
         yield idx
         yield idx + 1
+
+
+def virtual_vertical_neighbor_ids(id: int) -> Generator[int]:
+    yield id - 2
+    yield id + 2
+
+
+def virtual_horizontal_neighbor_ids(layout: Tuple[int, int], id: int) -> Generator[int]:
+    rows, _ = layout
+    if id % 2 == 0:
+        yield id - 1 - (2 * rows)
+        yield id + 1
+    else:
+        yield id - 1
+        yield id - 1 + (2 * rows)
+
+
+def physical_and_ancilla_from_virtual(virtual: int) -> Tuple[int, int]:
+    if virtual % 2 == 0:
+        return (2 * virtual, 2 * virtual + 2)
+    else:
+        return (2 * virtual + 1, 2 * virtual - 1)
+
+
+def schedule_virtual(
+    layout: Tuple[int, int], trace: List[Tuple[str, List[int]]]
+) -> List[List[Tuple[str, List[int]]]]:
+    tetron_trace = []
+    for event in trace:
+        name, targets = event
+        match name:
+            case "X" | "Y" | "Z":
+                name = ("C" * (len(targets) - 1)) + name
+        tetron_trace += decompose(layout, (name, targets))
+    return schedule_tetrons(layout, tetron_trace)
+
+
+def decompose(
+    layout: Tuple[int, int], event: Tuple[str, List[int]]
+) -> List[Tuple[str, List[int]]]:
+    name, targets = event
+    tetron_targets = [physical_and_ancilla_from_virtual(t) for t in targets]
+    match name:
+        case "X" | "Y" | "Z" | "T" | "M" | "Mx" | "My" | "MResetZ" | "Reset":
+            return [(name, [t[0] for t in tetron_targets])]
+        case "H":
+            tetron_target, tetron_ancilla = tetron_targets[0][0], tetron_targets[0][1]
+            return [
+                ("Mx", [tetron_ancilla]),
+                ("Mzy", [tetron_ancilla, tetron_target]),
+                ("My", [tetron_ancilla]),
+            ]
+        case "S":
+            tetron_target, tetron_ancilla = tetron_targets[0][0], tetron_targets[0][1]
+            return [
+                ("Mx", [tetron_ancilla]),
+                ("Mzz", [tetron_ancilla, tetron_target]),
+                ("My", [tetron_ancilla]),
+            ]
+        case "CX":
+            control, target = targets[0], targets[1]
+            assert target in virtual_horizontal_neighbor_ids(
+                layout, control
+            ), "CX must be between horizontal neighbors"
+            tetron_control, tetron_ancilla, tetron_target = (
+                tetron_targets[0][0],
+                tetron_targets[0][1],
+                tetron_targets[1][0],
+            )
+            return [
+                ("Mx", [tetron_ancilla]),
+                ("Mzz", [tetron_ancilla, tetron_control]),
+                ("Mxx", [tetron_ancilla, tetron_target]),
+                ("M", [tetron_ancilla]),
+            ]
+        case "CZ":
+            if targets[0] % 2 == 0:
+                control, target = min(targets[0], targets[1]), max(
+                    targets[0], targets[1]
+                )
+            else:
+                control, target = max(targets[0], targets[1]), min(
+                    targets[0], targets[1]
+                )
+            assert target in virtual_vertical_neighbor_ids(
+                control
+            ), "CZ must be between vertical neighbors"
+            tetron_control, tetron_ancilla = physical_and_ancilla_from_virtual(control)
+            tetron_target, _ = physical_and_ancilla_from_virtual(target)
+            return [
+                ("M", [tetron_ancilla]),
+                ("Mzy", [tetron_control, tetron_ancilla]),
+                ("Mzz", [tetron_ancilla, tetron_target]),
+                ("My", [tetron_ancilla]),
+            ]
+        case _:
+            raise ValueError(f"Unsupported gate: {name}")
